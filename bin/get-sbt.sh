8ae5c5a92c2c6906c06190034f85fd0b24c969d7
Christopher Nguyen <ctn@adatao.com>
2014-01-20 05:43:17 -0800

    [BUGFIX] Support curl in case wget is not available

--- bin/get-sbt.sh
+++ bin/get-sbt.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-cd `dirname $0` ; pwd
-
-[ -d sbt.dir ] && exit 0
-
 VERSION=0.13.1
 URL=http://repo.scala-sbt.org/scalasbt/sbt-native-packages/org/scala-sbt/sbt/$VERSION/sbt.zip
-wget -O /tmp/sbt.zip $URL
-rm -fr sbt ; unzip /tmp/sbt.zip ; mv sbt sbt.dir
+
+function usage {
+	echo ""
+	echo "    Usage: $0 [-f]"
+	echo ""
+	echo "    Attempts to retrieve SBT $VERSION from $URL."
+	echo "    If sbt.dir already exists, this will be skipped, unless the -f flag is specified."
+	echo ""
+	exit 1
+}
+
+cd `dirname $0` ; pwd
+
+[ "$1" != "-f" -a -d sbt.dir ] && usage
+
+if which curl ; then
+	curl  -o /tmp/sbt.zip $URL
+elif which wget ; then
+	wget -O /tmp/sbt.zip $URL
+else
+	echo "ERROR: must have either curl or wget to retrieve sbt from $URL"
+	exit 1
+fi
+
+rm -fr sbt sbt.dir ; unzip /tmp/sbt.zip ; mv sbt sbt.dir
 ln -s sbt.dir/bin/sbt sbt
