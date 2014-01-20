9678e2e1768c6a98610b2a25df0c2a0b919eb9e6
bhan <binhhan.gt@gmail.com>
2014-01-19 21:20:19 -0800

    Fix errors when wget or curl is not installed

--- bin/get-sbt.sh
+++ bin/get-sbt.sh
@@ -6,6 +6,14 @@ cd `dirname $0` ; pwd
 
 VERSION=0.13.1
 URL=http://repo.scala-sbt.org/scalasbt/sbt-native-packages/org/scala-sbt/sbt/$VERSION/sbt.zip
-wget -O /tmp/sbt.zip $URL
+if hash wget 2>/dev/null; then
+ wget -O /tmp/sbt.zip $URL
+elif hash curl 2>/dev/null; then
+ curl -o /tmp/sbt.zip $URL
+else
+ echo "You need curl or wget installed to download sbt."
+ exit
+fi
+
 rm -fr sbt ; unzip /tmp/sbt.zip ; mv sbt sbt.dir
 ln -s sbt.dir/bin/sbt sbt
