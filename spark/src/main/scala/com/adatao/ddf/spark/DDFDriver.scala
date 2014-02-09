89e93e2e4d1d2549bbfbac9beab2daedc3681f1d
Cuong Kien Bui <ckb@adatao.com>
2014-02-09 14:48:46 -0600

    Added a test for Spark DDFDriver.

--- spark/src/main/scala/com/adatao/ddf/spark/DDFDriver.scala
+++ spark/src/main/scala/com/adatao/ddf/spark/DDFDriver.scala
@@ -2,8 +2,12 @@ package com.adatao.ddf.spark
 
 import com.adatao.ddf.DDFDriver
 import com.adatao.ddf.DDF
+import com.adatao.ddf.DDFDriverManager
+import com.adatao.ddf.exception.DDFException
 
 class DDFDriver extends com.adatao.ddf.DDFDriver {
+  DDFDriver // to trigger the static call to register the driver with driver manager.
+  
   def createSchema(schemaString: java.lang.String): DDF = {
     return null
   }
@@ -14,5 +18,13 @@ class DDFDriver extends com.adatao.ddf.DDFDriver {
   
   def acceptURL(connectionURL: java.lang.String): Boolean = {
     return true
+  }
+}
+
+object DDFDriver {
+  try {
+    DDFDriverManager.registerDDFDriver(new com.adatao.ddf.spark.DDFDriver)
+  } catch {
+    case ex: DDFException => println(ex.getMessage)
   }
 }
---
+++ spark/src/test/scala/com/adatao/ddf/DDFDriverManagerSuite.scala
@@ -0,0 +1,7 @@
+package com.adatao.ddf
+
+class DDFDriverManagerSuite extends ATestSuite {
+  test("Test loading Spark Driver Manager") {
+    println("Test me")
+  }
+}
