657fb8964052f86a83676062e565f3fc14f403d0
Cuong Kien Bui <ckb@adatao.com>
2014-02-09 16:39:14 -0600

    Added unit test for creating a connection to a Spark cluster.

--- spark/src/test/scala/com/adatao/ddf/DDFDriverManagerSuite.scala
+++ spark/src/test/scala/com/adatao/ddf/DDFDriverManagerSuite.scala
@@ -11,4 +11,15 @@ class DDFDriverManagerSuite extends ATestSuite {
     val driver = DDFDriverManager.getDriver("spark:")
     println(driver)
   }
+  
+  test("Test connecting to a Spark Master") {
+    println("Test me")
+    new com.adatao.ddf.spark.DDFDriver
+    val driver = DDFDriverManager.getDriver("spark:")
+    val props = new java.util.HashMap[String, String]
+    props.put("spark.home", System.getProperty("SPARK_HOME"))
+    println(driver)
+    val ddfFactory = driver.connect("spark://ubuntu:7077", props)
+    println(ddfFactory)
+  }
 }
