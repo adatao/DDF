6e9803576c6667a89e695ee4f0aeb3c9bd69c879
Cuong Kien Bui <ckb@adatao.com>
2014-02-11 06:17:59 -0600

    Added examples for RClient module.

--- RClient/adatao.ddf/R/adatao.R
+++ RClient/adatao.ddf/R/adatao.R
@@ -19,16 +19,24 @@
 #'@examples
 #'
 #'## AdataoDataFrame
-#'#adatao.connect("localhost", 7911)
-#'#df <- adatao.LoadTable("resources/input")
-#'#colnames(df) <- c("V1","V2","V3")
-#'#summary(df)
-#'## AdataoVector
-#'#v <- df$V1
-#'#mean(v)
-#'## Linear regression
-#'#adatao.lm(V1 ~ V2 + V3, df)
-#'#adatao.disconnect()
+#'library(adatao.ddf)
+#'dr <- .jnew("com.adatao.ddf.spark.DDFDriver")
+#'dr
+#'props <- .jnew("java.util.HashMap")
+#'props$put("spark.home", "/home/cuongbk/Projects/AdataoProjects/Sandbox3/incubator-spark")
+#'props$get("spark.home")
+#'dr$connect("spark://ubuntu:7077", props)
+#'serverjar = c(.libPaths()[1], "/adatao.ddf/java/ddf_spark_2.9.3-1.0.jar")
+#'serverjar
+#'paste(serverjar, collapse = '')
+#'serverjar = paste(serverjar, collapse = '')
+#'serverjar
+#'props$put("RSERVER_JAR", serverjar)
+#'props
+#'ft <- v$connect("spark://ubuntu:7077", props)
+#'ft
+#'ddf <- ft$createSchema("abc")
+#'ddf
 #'
 #'@import rJava
 #'@import RJSONIO
