ba40b36e402bb4fe2d54249d5bf9efda7ca73469
bhan <bhan@adatao.com>
2014-02-13 20:32:07 -0800

    load hive table to ddf

--- spark/src/main/java/com/adatao/ddf/spark/SparkDDF.java
+++ spark/src/main/java/com/adatao/ddf/spark/SparkDDF.java
@@ -1,6 +1,12 @@
 package com.adatao.ddf.spark;
 
+import org.apache.spark.rdd.RDD;
+
+import shark.api.JavaTableRDD;
+import shark.memstore2.TablePartition;
+
 import com.adatao.ddf.DDF;
+import com.adatao.ddf.content.Schema;
 
 /**
  * SparkDDF extends DDF in order to provide some utility methods that are specific to Spark
@@ -16,5 +22,17 @@ public class SparkDDF extends DDF {
    * used within the spark module. They should not be used outside of this module.
    */
 
+  private RDD<TablePartition> mDataTable;
+  private Schema mSchema;
+  public SparkDDF (RDD<TablePartition> data) {
+    this.mDataTable = data;
+  }
+  public SparkDDF(RDD<TablePartition> data, Schema theSchema) {
+    this.mDataTable = data;
+    this.mSchema = theSchema;
+  }
+  public RDD<TablePartition> getRDD() {
+    return mDataTable;
+  }
 
 }
