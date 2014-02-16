419fe761ba288011c0a814b9d84a635ab41e8a34
huandao0812 <daoduchuan@gmail.com>
2014-02-17 01:33:57 +0700

    reformat code

--- spark/src/main/scala/com/adatao/spark/ddf/analytics/Kmeans.scala
+++ spark/src/main/scala/com/adatao/spark/ddf/analytics/Kmeans.scala
@@ -1,6 +1,6 @@
 package com.adatao.spark.ddf.analytics
 
-import com.adatao.ddf.analytics.{IAlgorithmOutputModel, IParameters, AAlgorithm, IAlgorithm}
+import com.adatao.ddf.analytics.{IAlgorithmOutputModel, IParameters, AAlgorithm}
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import com.adatao.ddf.DDF
 import org.apache.spark.rdd.RDD
@@ -8,7 +8,7 @@ import org.apache.spark.rdd.RDD
 /**
  * author: daoduchuan
  */
-class Kmeans(params: KmeansParameters) extends AAlgorithm(classOf[Array[Double]], params){
+class Kmeans(params: KmeansParameters) extends AAlgorithm(classOf[Array[Double]], params) {
 
   def this() = this(new KmeansParameters())
 
@@ -17,9 +17,9 @@ class Kmeans(params: KmeansParameters) extends AAlgorithm(classOf[Array[Double]]
   }
 
   override def run(ddf: DDF): KmeansModel = {
-    val data= ddf.getRepresentationHandler.get(this.getExpectedDataType).asInstanceOf[RDD[Array[Double]]]
+    val data = ddf.getRepresentationHandler.get(this.getExpectedDataType).asInstanceOf[RDD[Array[Double]]]
 
-    val model= KMeans.train(data, params.numCentroids, params.maxIter, params.runs, params.initMode)
+    val model = KMeans.train(data, params.numCentroids, params.maxIter, params.runs, params.initMode)
 
     new KmeansModel(ddf, params, model)
   }
@@ -28,7 +28,7 @@ class Kmeans(params: KmeansParameters) extends AAlgorithm(classOf[Array[Double]]
 class KmeansParameters(val numCentroids: Int = 10,
                        val maxIter: Int = 100,
                        val runs: Int = 10,
-                       val initMode: String ="random",
+                       val initMode: String = "random",
                        val epsilon: Double = 1e-3) extends IParameters
 
 class KmeansModel(val data: DDF, val params: KmeansParameters, val mllibKmeansModel: KMeansModel) extends IAlgorithmOutputModel {
