b79a26d8e12bd3a6f4bb24ebde8ccf8f178cbf83
bhan <binhhan.gt@gmail.com>
2014-02-26 18:09:40 -0800

    format aggregation result

--- spark/src/main/java/com/adatao/spark/ddf/analytics/AggregationHandler.java
+++ spark/src/main/java/com/adatao/spark/ddf/analytics/AggregationHandler.java
@@ -1,20 +1,23 @@
 package com.adatao.spark.ddf.analytics;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.adatao.ddf.DDF;
 import com.adatao.ddf.analytics.AAggregationHandler;
 import com.adatao.ddf.exception.DDFException;
 import com.adatao.ddf.util.Utils;
+
 /**
  * 
  * @author bhan
- *
+ * 
  */
-public class AggregationHandler extends AAggregationHandler{
+public class AggregationHandler extends AAggregationHandler {
   private static Logger logger = Logger.getLogger(AggregationHandler.class);
 
   public AggregationHandler(DDF theDDF) {
@@ -23,7 +26,8 @@ public class AggregationHandler extends AAggregationHandler{
 
   @Override
   public double corr(String colA, String colB) {
-    String sqlCmd = "Select corr(" + colA + ", " + colB + ") from "+ this.getDDF().getTableName();
+    String sqlCmd = "Select corr(" + colA + ", " + colB + ") from "
+        + this.getDDF().getTableName();
     List<String> rs;
     try {
       rs = this.getManager().sql2txt(sqlCmd);
@@ -33,8 +37,10 @@ public class AggregationHandler extends AAggregationHandler{
     }
     return 0;
   }
+
   @Override
-  public Map<String, Map<String, Double>> aggregate(String[] columnNames, String[] groupByColumns, String funcName) {
+  public Map<String, Double[]> aggregate(String[] columnNames,
+      String[] groupByColumns, String funcName) {
     String tableName = this.getDDF().getTableName();
     String groupByStr, funcStr;
     StringBuffer sb = new StringBuffer("");
@@ -43,32 +49,47 @@ public class AggregationHandler extends AAggregationHandler{
     }
     groupByStr = sb.toString();
     sb.setLength(0);
-    
+
     for (String col : columnNames) {
       sb.append(nameToFunction(col, funcName));
       sb.append(",");
     }
-    if (sb.length()>0) sb.setLength(sb.length()-1);// remove the right most ","
+    if (sb.length() > 0)
+      sb.setLength(sb.length() - 1);// remove the right most ","
     funcStr = sb.toString();
-    String sqlCmd = "Select " + groupByStr + funcStr + " from " + tableName + " group by " + groupByStr;
+    String sqlCmd = "Select " + groupByStr + funcStr + " from " + tableName
+        + " group by " + groupByStr;
     try {
       List<String> result = this.getManager().sql2txt(sqlCmd);
-      return toAggregateResult(result);
+      return toAggregateResult(result, groupByColumns.length);
     } catch (DDFException e) {
       logger.error("Unable to query from " + tableName, e);
     }
     return null;
   }
 
-  private Map<String, Map<String, Double>> toAggregateResult(List<String> result) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-  private String nameToFunction(String col, String funcName) {
-    // TODO Auto-generated method stub
+  private Map<String, Double[]> toAggregateResult(List<String> result, int len) {
+    Map<String, Double[]> aggregateResult = new HashMap<String, Double[]>();
+    for (String res : result) {
+      int pos = StringUtils.ordinalIndexOf(res, "\t", len);
+      String groupByColNames = res.substring(0, pos).replaceAll("\t", ",");
+      String[] stats = res.substring(pos + 1).split("\t");
+      Double[] statsDouble = new Double[stats.length];
+      for (int i = 0; i < stats.length; i++) {
+        statsDouble[i] = Utils.roundUp(Double.parseDouble(stats[i]));
+      }
+      aggregateResult.put(groupByColNames, statsDouble);
+    }
     return null;
   }
 
-  
-  
+  private String nameToFunction(String colName, String funcName) {
+    switch (AggregateFunction.valueOf(funcName)) {
+    case median:
+      return String.format(" percentile_approx(%s, 0.5),", colName);
+    default:
+      return funcName + String.format(" (%s),", colName);
+    }
+  }
+
 }
