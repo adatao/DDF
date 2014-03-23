35b6014582dff0fe472fc9490b2523a0933ddadb
kpham <khangich@gmail.com>
2014-03-23 22:31:33 +0700

    delete sh file

--- pa/test.sh
+++
@@ -1,2 +0,0 @@
-#mvn clean && mvn compile -DskipTests && mvn package -DskipTests && mvn -X test -Dtest=com.adatao.pa.spark.execution.RegressionSuite
-mvn clean && mvn compile -DskipTests && mvn package -DskipTests && mvn -X test -Dtest=SqlExecutionSuite > /tmp/a
