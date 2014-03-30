0ec5d76ef852a88f23ab2493d136599a8aeea922
Cuong Kien Bui <ckb@adatao.com>
2014-03-30 13:41:47 -0500

    Done with Kmeans executor migration.

--- pa/src/main/scala/com/adatao/pa/spark/execution/Kmeans.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/Kmeans.scala
@@ -10,6 +10,10 @@ import org.apache.spark.rdd.RDD
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.JavaConversions._
 import com.adatao.ML.Kmeans.{ParsePoint, SharkParsePoint, DataPoint}
+import java.util.List
+import java.util.ArrayList
+
+import com.adatao.ddf.DDF
 
 /**
  * Created with IntelliJ IDEA.
@@ -29,6 +33,33 @@ class Kmeans(
 		initialCentroids: java.util.List[Array[Double]] = null,
 		val initializationMode: String = "random")
 		extends AUnsupervisedTrainer[KmeansModel](dataContainerID, xCols){
+	override def train(dataContainerID: String, context: ExecutionContext): KmeansModel = {
+    val ddfManager = context.sparkThread.getDDFManager();
+    val ddf = ddfManager.getDDF(("SparkDDF-spark-" + dataContainerID).replace("-", "_")) match {
+      case x: DDF ⇒ x
+      case _ ⇒ throw new IllegalArgumentException("Only accept DDF")
+    }
+    // project the xCols, and yCol as a new DDF
+    // this is costly
+    val schema = ddf.getSchema()
+    var columnList : java.util.List[java.lang.String] = new java.util.ArrayList[java.lang.String]
+    for (col <- xCols) columnList.add(schema.getColumn(col).getName)
+    val projectDDF = ddf.Views.project(columnList)
+    val kmeansModel = projectDDF.ML.train("kmeans", K: java.lang.Integer, numIterations: java.lang.Integer)
+    // missing the methods to access weights
+    val rawModel = kmeansModel.getRawModel.asInstanceOf[org.apache.spark.mllib.clustering.KMeansModel]
+    
+    val totalWithins: ArrayBuffer[Double] = ArrayBuffer[Double]()
+    val pointsPerCluster: ArrayBuffer[Int] = ArrayBuffer[Int]()
+    for (i <- 1 to numIterations) {
+      totalWithins += 0
+    }
+    for (i <- 1 to K) {
+      pointsPerCluster += 0
+    }
+    return new KmeansModel(totalWithins.toArray, pointsPerCluster.toArray, rawModel.clusterCenters.toList, projectDDF.getNumRows())
+  }
+	
 	def train(dataPartition: RDD[Array[Double]], context: ExecutionContext): KmeansModel = {
 		type PointType = Tuple3[Array[Double], Int, Double]
 
