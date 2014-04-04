c3419281ca83b0a9a857550a95858d5ee78cbb07
kpham <khangich@gmail.com>
2014-04-04 09:04:25 -0700

    init

--- pa/src/main/scala/com/adatao/pa/spark/execution/ROC.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/ROC.scala
@@ -20,7 +20,7 @@ import com.adatao.ML.types.Matrix
 import com.adatao.ML.types.Vector
 import com.adatao.ML.TModel
 import com.adatao.ML.LogisticRegressionModel
-import com.adatao.ML.{Utils => MLUtils}
+import com.adatao.ML.{ Utils ⇒ MLUtils }
 import com.adatao.ML.spark.RddUtils
 import java.util.ArrayList
 import org.jblas.DoubleMatrix
@@ -33,67 +33,80 @@ import com.adatao.ML.LinearRegressionModel
 import java.util.HashMap
 import com.adatao.pa.AdataoException
 import com.adatao.pa.AdataoException.AdataoExceptionCode
+import com.adatao.ddf.DDF
 
 /**
  * From ctn: What is the intended API for ROC? Are we passing in a model and have it generate predictions, then compute
  * statistics for those prediction; or are we passing in predictions and expect it only to compute statistics for those
  * predictions?
- * 
- * Khang: it's the former. Get the data, do prediction, then compute statistic. 
- * 
+ *
+ * Khang: it's the former. Get the data, do prediction, then compute statistic.
+ *
  */
 
 class ROC(dataContainerID: String, xCols: Array[Int], var alpha_length: Int) extends AUnsupervisedTrainer[LinearRegressionModel](dataContainerID, xCols) {
 
 	def train(dataPartition: RDD[Array[Double]], ctx: ExecutionContext): RocObject = {
-			//first check if if input data is binary classification
-			val YTRUE_INDEX = 0
-			val df = ctx.sparkThread.getDataManager.get(dataContainerID)
-			val metaInfos= df.getMetaInfo
-			var isBinaryLabel = true
-			var isEmpty = false
-			
-			//if yTrue has factor
-			if(metaInfos != null && metaInfos(YTRUE_INDEX) != null && metaInfos(YTRUE_INDEX).hasFactor()) {
-				val yTrueIterator = metaInfos(YTRUE_INDEX).getFactor().keySet().iterator()
-				while(isBinaryLabel && yTrueIterator.hasNext()) {
-					val value = yTrueIterator.next()
-					if(value != "0" && value != "1")
-						isBinaryLabel = false
-				}
-			}
-			//if yTrue is not factor
-			else {
-				val isBinaryClassification = dataPartition.mapPartitions(checkBinaryClassification).collect
-				var i = 0
-				while (isBinaryLabel && i < isBinaryClassification.length) {
-					var j = 0
-					while (isBinaryLabel && j < isBinaryClassification(i).length) {
-						if(isBinaryClassification(i)(j) != 0 && isBinaryClassification(i)(j) != 1) {
-							isBinaryLabel = false
-						}
-						j += 1
-					}
-					i += 1
-				}
-				if (isBinaryClassification == null  || isBinaryClassification.length == 0) {
-					isEmpty = true
-				}
-			}
-			
-			if(isEmpty) {
-				if(metaInfos == null)
-					LOG.error("Predicted data is empty and metaInfos is null");
-				else 
-					LOG.error("Predicted data is empty and metaInfos =" + metaInfos);
-				throw new AdataoException(AdataoExceptionCode.ERR_ROC_EMPTY, "Please check if predicted data is empty.", null);
-			}
-			if(!isBinaryLabel) {
-				LOG.error("True label data is not binary classified data, please check input data");
-				throw new AdataoException(AdataoExceptionCode.ERR_ROC_NOT_BINARY, "Please make sure input data is binary classified.", null);
-			}
-			val data = dataPartition.mapPartitions(getData)
-			Metrics.ROC(data, alpha_length)
+		//first check if if input data is binary classification
+		//TODO double check if ytrueypred back by table i.e has schema
+		val YTRUE_INDEX = 0
+//		val df = ctx.sparkThread.getDataManager.get(dataContainerID)
+		val ddfManager = ctx.sparkThread.getDDFManager();
+		val ddf: DDF = ddfManager.getDDF(("SparkDDF-spark-" + dataContainerID).replace("-", "_"));
+		
+		
+//		val metaInfos = df.getMetaInfo
+		val columnName = ddf.getColumnName(YTRUE_INDEX)
+		val column = ddf.getColumn(columnName)
+		
+//		column.hasFactor?
+		var isBinaryLabel = true
+		var isEmpty = false
+
+		//if yTrue has factor
+//		if (column != null && column.hasFactor()) {
+		//TODO check factor in the original DDF
+//    if (column != null) {			
+//			val yTrueIterator = metaInfos(YTRUE_INDEX).getFactor().keySet().iterator()
+//			while (isBinaryLabel && yTrueIterator.hasNext()) {
+//				val value = yTrueIterator.next()
+//				if (value != "0" && value != "1")
+//					isBinaryLabel = false
+//			}
+//		}
+//		//if yTrue is not factor
+//		else {
+//			val isBinaryClassification = dataPartition.mapPartitions(checkBinaryClassification).collect
+//			var i = 0
+//			while (isBinaryLabel && i < isBinaryClassification.length) {
+//				var j = 0
+//				while (isBinaryLabel && j < isBinaryClassification(i).length) {
+//					if (isBinaryClassification(i)(j) != 0 && isBinaryClassification(i)(j) != 1) {
+//						isBinaryLabel = false
+//					}
+//					j += 1
+//				}
+//				i += 1
+//			}
+//			if (isBinaryClassification == null || isBinaryClassification.length == 0) {
+//				isEmpty = true
+//			}
+//		}
+
+//		if (isEmpty) {
+//			if (metaInfos == null)
+//				LOG.error("Predicted data is empty and metaInfos is null");
+//			else
+//				LOG.error("Predicted data is empty and metaInfos =" + metaInfos);
+//			throw new AdataoException(AdataoExceptionCode.ERR_ROC_EMPTY, "Please check if predicted data is empty.", null);
+//		}
+//		if (!isBinaryLabel) {
+//			LOG.error("True label data is not binary classified data, please check input data");
+//			throw new AdataoException(AdataoExceptionCode.ERR_ROC_NOT_BINARY, "Please make sure input data is binary classified.", null);
+//		}
+		
+		val data = dataPartition.mapPartitions(getData)
+		Metrics.ROC(data, alpha_length)
 	}
 
 	/*
@@ -104,20 +117,20 @@ class ROC(dataContainerID: String, xCols: Array[Int], var alpha_length: Int) ext
 		var numRows = 0
 		while (inputRows.hasNext) {
 			val aRow = inputRows.next
-					if (aRow != null) {
-						rows.append(aRow)
-						numRows += 1
-					}
+			if (aRow != null) {
+				rows.append(aRow)
+				numRows += 1
+			}
 		}
 		val X = new Array[Array[Double]](numRows)
 		var row = 0
 		rows.foreach(inputRow ⇒ {
-			X(row) = Array(inputRow(0), inputRow(1)) 
-					row += 1
+			X(row) = Array(inputRow(0), inputRow(1))
+			row += 1
 		})
 		Iterator(X)
 	}
-	
+
 	/*
 	 */
 	def checkBinaryClassification(inputRows: Iterator[Array[Double]]): Iterator[Array[Int]] = {
@@ -132,7 +145,7 @@ class ROC(dataContainerID: String, xCols: Array[Int], var alpha_length: Int) ext
 		while (inputRows.hasNext) {
 			val aRow = inputRows.next
 			if (aRow != null) {
-				if(aRow(YTRUE_INDEX) != 0 &&  aRow(YTRUE_INDEX) != 1 ){
+				if (aRow(YTRUE_INDEX) != 0 && aRow(YTRUE_INDEX) != 1) {
 					println(">>>isNotBinary=" + isNotBinary(0))
 					return (Iterator(isNotBinary))
 				}
