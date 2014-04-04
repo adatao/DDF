e609bc6ba9a264b46e7a7539650ce4c2617df071
kpham <khangich@gmail.com>
2014-04-03 17:16:40 -0700

    residuals compiled

--- pa/src/main/scala/com/adatao/pa/spark/execution/Residuals.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/Residuals.scala
@@ -4,6 +4,9 @@ import com.adatao.pa.spark.DataManager.{DataFrame, MetaInfo}
 import org.apache.spark.api.java.JavaRDD
 import com.adatao.pa.spark.SharkUtils
 import shark.api.JavaSharkContext
+import com.adatao.ddf.DDF
+import com.adatao.ddf.ml.IModel
+import com.adatao.spark.ddf.SparkDDF
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,20 +18,38 @@ import shark.api.JavaSharkContext
 class Residuals(dataContainerID: String, val modelID: String, val xCols: Array[Int], val yCol: Int)
 		extends AExecutor[ResidualsResult]{
 	override def runImpl(ctx: ExecutionContext): ResidualsResult = {
-		val ytrueypred= new YtrueYpred(dataContainerID, modelID, xCols, yCol)
-		val pred= ytrueypred.runImpl(ctx)
-		val predContainer= Option(ctx.sparkThread.getDataManager.get(pred.dataContainerID))
+//		val ytrueypred= new YtrueYpred(dataContainerID, modelID, xCols, yCol)
+//		val pred= ytrueypred.runImpl(ctx)
+//		val predContainer= Option(ctx.sparkThread.getDataManager.get(pred.dataContainerID))
 
-		val residuals= predContainer match {
-			case Some(df) =>  df match{
-				case dx: DataFrame =>	dx.getRDD.rdd.map{arr => arr match {
-						case Array(ytrue, ypred) => Array((ytrue.asInstanceOf[Double] - ypred.asInstanceOf[Double]).asInstanceOf[AnyRef])
-					}
-				}
-			}
-			case None => throw new IllegalArgumentException("dataContainerID doesn't exist in user session")
-		}
+		
+		val ddfManager = ctx.sparkThread.getDDFManager();
+    val ddf: DDF = ddfManager.getDDF(("SparkDDF-spark-" + dataContainerID).replace("-", "_"));
 
+    // first, compute RDD[(ytrue, ypred)]
+    //all we need is to change it HERE
+    //old API
+//    val predictions = getYtrueYpred(dataContainerID, modelID, xCols, yCol, ctx)
+    
+    val mymodel: IModel = ddfManager.getModel(modelID)
+    val resultDDF = ddf.getMLSupporter().applyModel(mymodel, true, true)
+    val predictionsRDD = resultDDF.asInstanceOf[SparkDDF].getRDD(classOf[Array[Double]])
+    
+    val residuals = predictionsRDD .map{arr => arr match {
+            case Array(ytrue, ypred) => Array((ytrue.asInstanceOf[Double] - ypred.asInstanceOf[Double]).asInstanceOf[AnyRef])
+    }    
+    }
+//		val residuals= predictionsRDD match {
+//			case Some(df) =>  df match{
+//				case dx: DataFrame =>	dx.getRDD.rdd.map{arr => arr match {
+//						case Array(ytrue, ypred) => Array((ytrue.asInstanceOf[Double] - ypred.asInstanceOf[Double]).asInstanceOf[AnyRef])
+//					}
+//				}
+//			}
+//			case None => throw new IllegalArgumentException("dataContainerID doesn't exist in user session")
+//		}
+
+    //return dataframe
 		val metaInfo= Array(new MetaInfo("residual", "java.lang.Double"))
 		val jsc = ctx.sparkThread.getSparkContext.asInstanceOf[JavaSharkContext]
 		val sdf= SharkUtils.createSharkDataFrame(new DataFrame(metaInfo, JavaRDD.fromRDD(residuals)), jsc)
