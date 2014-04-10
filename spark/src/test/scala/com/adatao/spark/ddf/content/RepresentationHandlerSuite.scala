6b0273a9473353d03f7da16a56422161cce22c24
huandao0812 <huan@adatau.com>
2014-04-10 12:07:45 -0700

    add test for Row and TablePartition

--- spark/src/test/scala/com/adatao/spark/ddf/content/RepresentationHandlerSuite.scala
+++ spark/src/test/scala/com/adatao/spark/ddf/content/RepresentationHandlerSuite.scala
@@ -1,7 +1,9 @@
 package com.adatao.spark.ddf.content
 
-import com.adatao.spark.ddf.{SparkDDFManager, ATestSuite}
+import com.adatao.spark.ddf.{SparkDDF, SparkDDFManager, ATestSuite}
 import com.adatao.ddf.DDFManager
+import shark.api.Row
+import shark.memstore2.TablePartition
 
 /**
  * author: daoduchuan
@@ -11,6 +13,16 @@ class RepresentationHandlerSuite extends ATestSuite {
   test("  ") {
     val manager = DDFManager.get("spark").asInstanceOf[SparkDDFManager]
     createTableAirline(manager.getSharkContext)
-    val ddf = manager.sql2ddf("select * from airline")
+    val ddf = manager.sql2ddf("select * from airline").asInstanceOf[SparkDDF]
+    val rddRow = ddf.getRDD(classOf[Row])
+    val rddTablePartition = ddf.getRDD(classOf[TablePartition])
+
+    assert(rddRow != null, "Can get RDD[Row]")
+    assert(rddTablePartition != null, "Can get RDD[TablePartition]")
+    assert(rddRow.count() == 301)
+    rddTablePartition.count()
+    /*rddTablePartition.foreach{
+      tp => assert(tp.isInstanceOf[TablePartition])
+    } */
   }
 }
