7c3f92f1b1c69795f72954e5e4cf741fd80e482c
khangich <khangich@gmail.com>
2014-05-20 11:38:43 -0700

    disable HadoopFsShell

--- pa/src/test/scala/com/adatao/pa/spark/execution/HadoopFsShellSuite.scala
+++ pa/src/test/scala/com/adatao/pa/spark/execution/HadoopFsShellSuite.scala
@@ -4,7 +4,7 @@ import com.adatao.pa.spark.types.ABigRClientTest
 import com.adatao.pa.spark.execution.HadoopFsShell._
 
 class HadoopFsShellSuite extends ABigRClientTest {
-	test("test HadoopFsShell") {
+	ignore("test HadoopFsShell") {
 		// Test ls
 		val cmd1 = new HadoopFsShell("ls", "/")
 		val res1 = bigRClient.execute[HadoopFsShellResult](cmd1)
