53826f3335ca57b0edd5e56f745ab839abda3dba
huandao0812 <huan@adatau.com>
2014-06-10 04:58:57 -0700

    fix bug in MLSupporter

--- pa/src/main/scala/com/adatao/pa/spark/execution/Serialize2DDF.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/Serialize2DDF.scala
@@ -4,6 +4,8 @@ import com.adatao.pa.spark.Utils.DataFrameResult
 import com.adatao.basic.ddf.BasicDDF
 import com.adatao.pa.spark.Utils
 import com.adatao.pa.spark.DataManager.MetaInfo
+import com.adatao.pa.AdataoException
+import com.adatao.pa.AdataoException.AdataoExceptionCode
 
 /**
   */
@@ -14,8 +16,11 @@ class Serialize2DDF(modelID: String) extends AExecutor[DataFrameResult] {
   protected override def runImpl(context: ExecutionContext): DataFrameResult = {
     val manager = context.sparkThread.getDDFManager
     val model = manager.getModel(modelID)
-    val newDDF = new BasicDDF(model)
+    if(model == null) {
+      throw new AdataoException(AdataoExceptionCode.ERR_GENERAL, "model not founded", null);
+    }
 
+    val newDDF = new BasicDDF(model)
     val name = manager.addDDF(newDDF)
     LOG.info(">>>>> put ddf to manager: " + name)
 
