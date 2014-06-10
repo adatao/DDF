99e9e6599648ff71867ef94328eb73b3e93bd1ae
khangich <khangich@gmail.com>
2014-06-10 08:16:47 +0700

    wip

---
+++ spark/src/main/java/com/adatao/spark/ddf/HBaseDDFManager.java
@@ -0,0 +1,372 @@
+package com.adatao.spark.ddf;
+
+
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import shark.SharkContext;
+import shark.SharkEnv;
+import shark.api.JavaSharkContext;
+import com.adatao.ddf.DDF;
+import com.adatao.ddf.DDFManager;
+import com.adatao.ddf.exception.DDFException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.spark.SparkContext;
+import org.apache.spark.rdd.RDD;
+import scala.Tuple2;
+import shark.SharkEnv;
+import shark.api.JavaSharkContext;
+import com.adatao.ddf.DDF;
+import com.adatao.ddf.DDFManager;
+import com.adatao.ddf.exception.DDFException;
+
+/**
+ * An Apache-Spark-based implementation of DDFManager
+ */
+public class HBaseDDFManager extends DDFManager {
+
+  @Override
+  public String getEngine() {
+    return "hbase";
+  }
+
+
+  private static final String DEFAULT_SPARK_APPNAME = "DDFClient";
+  private static final String DEFAULT_SPARK_MASTER = "local[4]";
+
+
+
+  public HBaseDDFManager(SparkContext sparkContext) throws DDFException {
+    this.initialize(sparkContext, null);
+  }
+
+  /**
+   * Use system environment variables to configure the SparkContext creation.
+   * 
+   * @throws DDFException
+   */
+  public HBaseDDFManager() throws DDFException {
+    this.initialize(null, new HashMap<String, String>());
+  }
+
+  public HBaseDDFManager(Map<String, String> params) throws DDFException {
+    this.initialize(null, params);
+  }
+
+  private void initialize(SparkContext sparkContext, Map<String, String> params) throws DDFException {
+    this.setSparkContext(sparkContext == null ? this.createSparkContext(params) : sparkContext);
+
+    //it will never go here
+    //TODO remove later
+    if (sparkContext instanceof SharkContext) {
+      this.setSharkContext((SharkContext) sparkContext);
+      sparkContext.conf().set("spark.kryo.registrator", "com.adatao.spark.content.KryoRegistrator");
+      mSharkContext.conf().set("spark.kryo.registrator", "com.adatao.spark.content.KryoRegistrator");
+      mSparkContext.conf().set("spark.kryo.registrator", "com.adatao.spark.content.KryoRegistrator");
+      
+      mLog.info(">>>>>>>>>>>>> setting Kryo for mSharkContext: " + mSharkContext.conf().get("spark.kryo.registrator"));
+    }
+  }
+
+
+  public DDF loadTable(String tableName, String fieldSeparator) throws DDFException {
+//  JavaRDD<String> fileRDD = mJavaSharkContext.textFile(fileURL);
+  
+  Configuration config = HBaseConfiguration.create();
+
+//  config.set("hbase.zookeeper.znode.parent", "hostname1");
+//  config.set("hbase.zookeeper.quorum","hostname1");
+//  config.set("hbase.zookeeper.property.clientPort","2181");
+  config.set("hbase.master", "localhost:");
+//  config.set("fs.defaultFS","hdfs://hostname1/");
+//  config.set("dfs.namenode.rpc-address","localhost:8020");
+
+  config.set(TableInputFormat.INPUT_TABLE, tableName);
+
+// JavaSparkContext ctx = new JavaSparkContext(args[0], "Simple",
+//       System.getenv(sparkHome), JavaSparkContext.jarOfClass(Simple.class));
+
+  RDD<Tuple2<ImmutableBytesWritable, Result>> hBaseRDD  = getSparkContext().newAPIHadoopRDD( config, TableInputFormat.class,
+          ImmutableBytesWritable.class, Result.class);
+  
+  long a = hBaseRDD.count();
+  System.out.println(">>>> count table = " + a);
+  DDF ddf = null;
+  return ddf;
+}
+
+  public String getDDFEngine() {
+    return "hbase";
+  }
+
+
+  private SparkContext mSparkContext;
+
+
+  public SparkContext getSparkContext() {
+    return mSparkContext;
+  }
+
+  private void setSparkContext(SparkContext sparkContext) {
+    this.mSparkContext = sparkContext;
+  }
+
+
+  private SharkContext mSharkContext;
+
+
+  public SharkContext getSharkContext() {
+    return mSharkContext;
+  }
+
+  private JavaSharkContext mJavaSharkContext;
+  
+  
+  public JavaSharkContext getJavaSharkContext() {
+    return mJavaSharkContext;
+  }
+
+  public void setJavaSharkContext(JavaSharkContext javaSharkContext) {
+    this.mJavaSharkContext = javaSharkContext;
+  }
+
+  /**
+   * Also calls setSparkContext() to the same sharkContext
+   * 
+   * @param sharkContext
+   */
+  private void setSharkContext(SharkContext sharkContext) {
+    this.mSharkContext = sharkContext;
+    this.setSparkContext(sharkContext);
+  }
+
+
+  private Map<String, String> mSparkContextParams;
+
+
+  public Map<String, String> getSparkContextParams() {
+    return mSparkContextParams;
+  }
+
+  private void setSparkContextParams(Map<String, String> mSparkContextParams) {
+    this.mSparkContextParams = mSparkContextParams;
+  }
+
+
+
+  public void shutdown() {
+    if (this.getSharkContext() != null) {
+      this.getSharkContext().stop();
+    } else if (this.getSparkContext() != null) {
+      this.getSparkContext().stop();
+    }
+  }
+
+
+  private static final String[][] SPARK_ENV_VARS = new String[][] { 
+    // @formatter:off
+    { "SPARK_APPNAME", "spark.appname" },
+    { "SPARK_MASTER", "spark.master" }, 
+    { "SPARK_HOME", "spark.home" }, 
+    { "SPARK_SERIALIZER", "spark.serializer" },
+    { "SPARK_SERIALIZER", "spark.serializer" },
+    { "HIVE_HOME", "hive.home" },
+    { "HADOOP_HOME", "hadoop.home" },
+    { "DDFSPARK_JAR", "ddfspark.jar" } 
+    // @formatter:on
+  };
+
+
+  /**
+   * Takes an existing params map, and reads both environment as well as system property settings to merge into it. The
+   * merge priority is as follows: (1) already set in params, (2) in system properties (e.g., -Dspark.home=xxx), (3) in
+   * environment variables (e.g., export SPARK_HOME=xxx)
+   * 
+   * @param params
+   * @return
+   */
+  private Map<String, String> mergeSparkParamsFromSettings(Map<String, String> params) {
+    if (params == null) params = new HashMap<String, String>();
+
+    Map<String, String> env = System.getenv();
+
+    for (String[] varPair : SPARK_ENV_VARS) {
+      if (params.containsKey(varPair[0])) continue; // already set in params
+
+      // Read setting either from System Properties, or environment variable.
+      // Env variable has lower priority if both are set.
+      String value = System.getProperty(varPair[1], env.get(varPair[0]));
+      if (value != null && value.length() > 0) params.put(varPair[0], value);
+    }
+
+    // Some well-known defaults
+    if (!params.containsKey("SPARK_MASTER")) params.put("SPARK_MASTER", DEFAULT_SPARK_MASTER);
+    if (!params.containsKey("SPARK_APPNAME")) params.put("SPARK_APPNAME", DEFAULT_SPARK_APPNAME);
+
+    return params;
+  }
+
+  /**
+   * Side effect: also sets SharkContext and SparkContextParams in case the client wants to examine or use those.
+   * 
+   * @param params
+   * @return
+   * @throws DDFException
+   */
+  private SparkContext createSparkContext(Map<String, String> params) throws DDFException {
+    this.setSparkContextParams(this.mergeSparkParamsFromSettings(params));
+    String ddfSparkJar = params.get("DDFSPARK_JAR");
+    String[] jobJars = ddfSparkJar != null ? ddfSparkJar.split(",") : new String[] {};
+
+    mJavaSharkContext = new JavaSharkContext(params.get("SPARK_MASTER"), params.get("SPARK_APPNAME"),
+        params.get("SPARK_HOME"), jobJars, params);
+    this.setSharkContext(SharkEnv.initWithJavaSharkContext(mJavaSharkContext).sharkCtx());
+
+    //set up serialization from System property
+  mSharkContext.conf().set("spark.kryo.registrator", System.getProperty("spark.kryo.registrator", "com.adatao.spark.content.KryoRegistrator"));
+  mSparkContext.conf().set("spark.kryo.registrator", System.getProperty("spark.kryo.registrator", "com.adatao.spark.content.KryoRegistrator"));
+    
+  return this.getSparkContext();
+  }
+  
+  /**
+   * Given a String[] vector of data values along one column, try to infer what the data type should be.
+   * 
+   * TODO: precompile regex
+   * 
+   * @param vector
+   * @return string representing name of the type "integer", "double", "character", or "logical" The algorithm will
+   *         first scan the vector to detect whether the vector contains only digits, ',' and '.', <br>
+   *         if true, then it will detect whether the vector contains '.', <br>
+   *         &nbsp; &nbsp; if true then the vector is double else it is integer <br>
+   *         if false, then it will detect whether the vector contains only 'T' and 'F' <br>
+   *         &nbsp; &nbsp; if true then the vector is logical, otherwise it is characters
+   */
+  public static String determineType(String[] vector, Boolean doPreferDouble) {
+    boolean isNumber = true;
+    boolean isInteger = true;
+    boolean isLogical = true;
+    boolean allNA = true;
+
+    for (String s : vector) {
+      if (s == null || s.startsWith("NA") || s.startsWith("Na") || s.matches("^\\s*$")) {
+        // Ignore, don't set the type based on this
+        continue;
+      }
+
+      allNA = false;
+
+      if (isNumber) {
+        // match numbers: 123,456.123 123 123,456 456.123 .123
+        if (!s.matches("(^|^-)((\\d+(,\\d+)*)|(\\d*))\\.?\\d+$")) {
+          isNumber = false;
+        }
+        // match double
+        else if (isInteger && s.matches("(^|^-)\\d*\\.{1}\\d+$")) {
+          isInteger = false;
+        }
+      }
+
+      // NOTE: cannot use "else" because isNumber changed in the previous
+      // if block
+      if (isLogical) {
+        if (!s.toLowerCase().matches("^t|f|true|false$")) {
+          isLogical = false;
+        }
+      }
+    }
+
+    // String result = "Unknown";
+    String result = "string";
+
+    if (!allNA) {
+      if (isNumber) {
+        if (!isInteger || doPreferDouble) {
+          result = "double";
+        }
+        else {
+          result = "int";
+        }
+      }
+      else {
+        if (isLogical) {
+          result = "boolean";
+        }
+        else {
+          result = "string";
+        }
+      }
+    }
+    return result;
+  }
+  
+  /**
+   * TODO: check more than a few lines in case some lines have NA
+   * 
+   * @param fileRDD
+   * @return
+   */
+  public String[] getMetaInfo(JavaRDD<String> fileRDD, String fieldSeparator) {
+    String[] headers = null;
+    int sampleSize = 5;
+
+    // sanity check
+    if (sampleSize < 1) {
+      mLog.info("DATATYPE_SAMPLE_SIZE must be bigger than 1");
+      return null;
+    }
+
+    List<String> sampleStr = fileRDD.take(sampleSize);
+    sampleSize = sampleStr.size(); // actual sample size
+    mLog.info("Sample size: " + sampleSize);
+
+    // create sample list for getting data type
+    String[] firstSplit = sampleStr.get(0).split(fieldSeparator);
+
+    // get header
+    boolean hasHeader = false;
+    if (hasHeader) {
+      headers = firstSplit;
+    }
+    else {
+      headers = new String[firstSplit.length];
+      for (int i = 0; i < headers.length;) {
+        headers[i] = "V" + (++i);
+      }
+    }
+
+    String[][] samples = hasHeader ? (new String[firstSplit.length][sampleSize - 1])
+        : (new String[firstSplit.length][sampleSize]);
+
+    String[] metaInfoArray = new String[firstSplit.length];
+    int start = hasHeader ? 1 : 0;
+    for (int j = start; j < sampleSize; j++) {
+      firstSplit = sampleStr.get(j).split(fieldSeparator);
+      for (int i = 0; i < firstSplit.length; i++) {
+        samples[i][j - start] = firstSplit[i];
+      }
+    }
+
+    boolean doPreferDouble = true;
+    for (int i = 0; i < samples.length; i++) {
+      String[] vector = samples[i];
+      metaInfoArray[i] = headers[i] + " " + determineType(vector, doPreferDouble);
+    }
+
+    return metaInfoArray;
+  }
+}
---
+++ spark/src/test/java/com/adatao/spark/ddf/HBaseDDFManagerTests.java
@@ -0,0 +1,182 @@
+package com.adatao.spark.ddf;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import com.adatao.ddf.DDFManager;
+import com.adatao.ddf.exception.DDFException;
+
+public class HBaseDDFManagerTests {
+
+  private static Configuration conf = null;
+  /**
+   * Initialization
+   */
+  static {
+    conf = HBaseConfiguration.create();
+  }
+
+
+  /**
+   * Create a table
+   */
+  public static void creatTable(String tableName, String[] familys) throws Exception {
+    HBaseAdmin admin = new HBaseAdmin(conf);
+    if (admin.tableExists(tableName)) {
+      System.out.println("table already exists!");
+    } else {
+      HTableDescriptor tableDesc = new HTableDescriptor(tableName);
+      for (int i = 0; i < familys.length; i++) {
+        tableDesc.addFamily(new HColumnDescriptor(familys[i]));
+      }
+      admin.createTable(tableDesc);
+      System.out.println("create table " + tableName + " ok.");
+    }
+  }
+
+  /**
+   * Delete a table
+   */
+  public static void deleteTable(String tableName) throws Exception {
+    try {
+      HBaseAdmin admin = new HBaseAdmin(conf);
+      admin.disableTable(tableName);
+      admin.deleteTable(tableName);
+      System.out.println("delete table " + tableName + " ok.");
+    } catch (MasterNotRunningException e) {
+      e.printStackTrace();
+    } catch (ZooKeeperConnectionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Put (or insert) a row
+   */
+  public static void addRecord(String tableName, String rowKey, String family, String qualifier, String value)
+      throws Exception {
+    try {
+      HTable table = new HTable(conf, tableName);
+      Put put = new Put(Bytes.toBytes(rowKey));
+      put.add(Bytes.toBytes(family), Bytes.toBytes(qualifier), Bytes.toBytes(value));
+      table.put(put);
+      System.out.println("insert recored " + rowKey + " to table " + tableName + " ok.");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Delete a row
+   */
+  public static void delRecord(String tableName, String rowKey) throws IOException {
+    HTable table = new HTable(conf, tableName);
+    List<Delete> list = new ArrayList<Delete>();
+    Delete del = new Delete(rowKey.getBytes());
+    list.add(del);
+    table.delete(list);
+    System.out.println("del recored " + rowKey + " ok.");
+  }
+
+  /**
+   * Get a row
+   */
+  public static void getOneRecord(String tableName, String rowKey) throws IOException {
+    HTable table = new HTable(conf, tableName);
+    Get get = new Get(rowKey.getBytes());
+    Result rs = table.get(get);
+    for (KeyValue kv : rs.raw()) {
+      System.out.print(new String(kv.getRow()) + " ");
+      System.out.print(new String(kv.getFamily()) + ":");
+      System.out.print(new String(kv.getQualifier()) + " ");
+      System.out.print(kv.getTimestamp() + " ");
+      System.out.println(new String(kv.getValue()));
+    }
+  }
+
+  /**
+   * Scan (or list) a table
+   */
+  public static void getAllRecord(String tableName) {
+    try {
+      HTable table = new HTable(conf, tableName);
+      Scan s = new Scan();
+      ResultScanner ss = table.getScanner(s);
+      for (Result r : ss) {
+        for (KeyValue kv : r.raw()) {
+          System.out.print(new String(kv.getRow()) + " ");
+          System.out.print(new String(kv.getFamily()) + ":");
+          System.out.print(new String(kv.getQualifier()) + " ");
+          System.out.print(kv.getTimestamp() + " ");
+          System.out.println(new String(kv.getValue()));
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testLongSparkDDFManagerRetrieval() throws DDFException {
+    try {
+      HBaseDDFManager manager = (HBaseDDFManager) DDFManager.get("hbase");
+      Assert.assertEquals("hbase", manager.getEngine());
+      
+//      HBaseDDFManager hb = (HBaseDDFManager) manager;
+//      manager.loadTable("test", ",");
+
+
+//      String tablename = "scores";
+//      String[] familys = { "grade", "course" };
+//      HBaseDDFManagerTests.creatTable(tablename, familys);
+//
+//      System.out.println(">>>>>>>> after create table");
+//
+//      // add record zkb
+//      HBaseDDFManagerTests.addRecord(tablename, "zkb", "grade", "", "5");
+//      HBaseDDFManagerTests.addRecord(tablename, "zkb", "course", "", "90");
+//      HBaseDDFManagerTests.addRecord(tablename, "zkb", "course", "math", "97");
+//      HBaseDDFManagerTests.addRecord(tablename, "zkb", "course", "art", "87");
+//      // add record baoniu
+//      HBaseDDFManagerTests.addRecord(tablename, "baoniu", "grade", "", "4");
+//      HBaseDDFManagerTests.addRecord(tablename, "baoniu", "course", "math", "89");
+//
+//      System.out.println("===========get one record========");
+//      HBaseDDFManagerTests.getOneRecord(tablename, "zkb");
+//
+//      System.out.println("===========show all record========");
+//      HBaseDDFManagerTests.getAllRecord(tablename);
+//
+//      System.out.println("===========del one record========");
+//      HBaseDDFManagerTests.delRecord(tablename, "baoniu");
+//      HBaseDDFManagerTests.getAllRecord(tablename);
+//
+//      System.out.println("===========show all record========");
+//      HBaseDDFManagerTests.getAllRecord(tablename);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
