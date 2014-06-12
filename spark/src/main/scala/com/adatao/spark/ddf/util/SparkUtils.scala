65843c9cb5a08984e218010d917260121b7bf932
huandao0812 <huan@adatau.com>
2014-06-12 15:18:14 -0700

    add comment

--- spark/src/main/scala/com/adatao/spark/ddf/util/SparkUtils.scala
+++ spark/src/main/scala/com/adatao/spark/ddf/util/SparkUtils.scala
@@ -9,9 +9,17 @@ import org.apache.spark.SparkConf
  */
 
 object SparkUtils {
+  /**
+   *  Create custom sharkContext with adatao's spark.kryo.registrator
+   * @param master
+   * @param jobName
+   * @param sparkHome
+   * @param jars
+   * @param environment
+   * @return
+   */
   def createSparkConf(master: String, jobName: String, sparkHome: String, jars: Array[String],
                       environment: JMap[String, String]): SparkConf = {
-
     val conf = SharkContext.createSparkConf(master, jobName, sparkHome, jars, environment.asScala)
     conf.set("spark.kryo.registrator", System.getProperty("spark.kryo.registrator", classOf[KryoRegistrator].getName))
   }
