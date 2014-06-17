8bdb82ccc2a9c8b3cb1c49836180026d630a431c
huandao0812 <huan@adatau.com>
2014-06-17 14:46:48 -0700

    add column information

--- pa/src/main/scala/com/adatao/pa/spark/execution/Kmeans.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/Kmeans.scala
@@ -6,6 +6,8 @@ import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.rdd.RDD
 import com.adatao.ddf.DDF
 import com.adatao.spark.ddf.content.RepresentationHandler
+import com.adatao.ddf.ml.IModel
+import org.apache.spark.mllib.clustering.KMeansModel
 
 class Kmeans(
   dataContainerID: String,
@@ -16,11 +18,11 @@ class Kmeans(
   val initializationMode: String,
   var initializationSteps: Int = 5,
   var epsilon: Double = 1e-4)
-  extends AExecutor[org.apache.spark.mllib.clustering.KMeansModel](true) {
+  extends AExecutor[KmeansModel](true) {
   
   override def runImpl(ctx: ExecutionContext) = train(dataContainerID, ctx)
   
-  def train(dataContainerID: String, context: ExecutionContext): org.apache.spark.mllib.clustering.KMeansModel = {
+  def train(dataContainerID: String, context: ExecutionContext): KmeansModel = {
     val ddfManager = context.sparkThread.getDDFManager();
     val ddf = ddfManager.getDDF(("SparkDDF-spark-" + dataContainerID).replace("-", "_")) match {
       case x: DDF ⇒ x
@@ -35,29 +37,18 @@ class Kmeans(
     val kmeansModel = projectedDDF.ML.train("kmeans", K: java.lang.Integer, numIterations: java.lang.Integer)
 
     // converts DDF model to old PA model
-    val rawModel = kmeansModel.getRawModel.asInstanceOf[org.apache.spark.mllib.clustering.KMeansModel]
-    rawModel
-
-    /*    val wcss = rawModel.computeCost(projectedDDF.getRepresentationHandler().get(RepresentationHandler.RDD_MLLIB_VECTOR).asInstanceOf[RDD[org.apache.spark.mllib.linalg.Vector]])
-    val totalWithins: ArrayBuffer[Double] = ArrayBuffer[Double]()
-
-    val pointsPerCluster: ArrayBuffer[Int] = ArrayBuffer[Int]()
-    for (i ← 1 to numIterations-1) {
-      totalWithins += 0
-    }
-    totalWithins += wcss
-    for (i ← 1 to K) {
-      pointsPerCluster += 0
-    }
-    return new KmeansModel(totalWithins.toArray, pointsPerCluster.toArray, rawModel.clusterCenters.toList, projectedDDF.getNumRows())*/
+//    val rawModel = kmeansModel.getRawModel.asInstanceOf[org.apache.spark.mllib.clustering.KMeansModel]
+//    rawModel
+    return new KmeansModel(kmeansModel.getTrainedColumns, kmeansModel.getRawModel.asInstanceOf[KMeansModel])
   }
-
 }
 
 object Kmeans {
   val RANDOM = "random"
   val K_MEANS_PARALLEL = "k-means||"
 }
+
+class KmeansModel(val trainedColumns: Array[String], val model: KMeansModel)
 
 
 
