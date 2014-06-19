884d22bee744ede7c31a2d3026842e78a82df7cf
root <root@ip-10-235-66-82.ec2.internal>
2014-06-19 16:54:59 +0000

    change script deploy

--- pa/exe/build_and_deploy_jars.sh
+++ pa/exe/build_and_deploy_jars.sh
@@ -19,11 +19,11 @@ bin/sbt 'project pa' assembly
 
 echo "# copy jars to slaves, and put assembly fat jar to hdfs #"
 
-/root/spark-ec2/copy-dir.sh $DDF_HOME
+/root/spark-ec2/copy-dir.sh $DDF_HOME &
 ${HADOOP_HOME}/bin/hdfs dfs -rmr /user/root/ddf_pa-assembly-0.9.jar
 
 echo "# put assembly fat jar to hdfs #"
 ${HADOOP_HOME}/bin/hdfs dfs -put ${PA_HOME}/target/scala-2.10/ddf_pa-assembly-0.9.jar /user/root
-
+wait
 echo "# THANK YOU, DONE #"
 
