3f1f697080a6223c7e30f8f85403f1ba4d3f32c1
bhan <binhhan.gt@gmail.com>
2014-06-25 15:05:54 -0700

    test pa executors

---
+++ pa/src/test/java/com/adatao/pa/spark/execution/TestMissingDataHandling.java
@@ -0,0 +1,64 @@
+package com.adatao.pa.spark.execution;
+
+import java.util.Arrays;
+import junit.framework.Assert;
+import org.apache.thrift.TException;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.adatao.pa.spark.DataManager.MetaInfo;
+import com.adatao.pa.spark.execution.NRow.NRowResult;
+import com.adatao.pa.spark.types.ExecutionResult;
+import com.adatao.pa.thrift.generated.JsonCommand;
+import com.adatao.pa.thrift.generated.JsonResult;
+import com.google.gson.Gson;
+
+public class TestMissingDataHandling extends BaseTest {
+  public static Logger LOG = LoggerFactory.getLogger(TestGroupBy.class);
+
+  String airlineID;
+  String sid;
+  @Before
+  public void init() throws TException {
+    JsonCommand cmd = new JsonCommand().setCmdName("connect");
+    Gson gson = new Gson();
+
+    JsonResult res = client.execJsonCommand(cmd);
+    sid = res.sid;
+    LOG.info("Got session ID: " + sid);
+    assert (sid != null);
+
+    createTableAirlineWithNA(sid);
+
+    Sql2DataFrame.Sql2DataFrameResult ddf = this.runSQL2RDDCmd(sid, "SELECT * FROM mtcars", true);
+    airlineID = ddf.dataContainerID;
+  }
+  
+  @Test
+  public void testDropNA() throws TException {
+    JsonCommand cmd = new JsonCommand();
+    JsonResult res;
+    com.adatao.pa.spark.Utils.DataFrameResult groupbyResult;
+    NRowResult nrResult;
+    MetaInfo[] metaInfo;
+
+    res = client.execJsonCommand(cmd
+        .setSid(sid)
+        .setCmdName("DropNA")
+        .setParams(
+            String.format("{dataContainerID: %s," + "Axis: row," + "NAChecking: any," + "thresh: 0," + "columns: null}",
+                airlineID)));
+    groupbyResult = ExecutionResult.fromJson(res.getResult(), com.adatao.pa.spark.Utils.DataFrameResult.class).result();
+
+    metaInfo = groupbyResult.getMetaInfo();
+    LOG.info("DropNA result: " + Arrays.toString(metaInfo));
+
+    Assert.assertTrue(metaInfo[0].getHeader().equals("year"));
+    res = client.execJsonCommand(cmd.setSid(sid).setCmdName("NRow")
+        .setParams(String.format("{dataContainerID: %s}", groupbyResult.getDataContainerID())));
+    nrResult = ExecutionResult.fromJson(res.getResult(), NRowResult.class).result();
+    Assert.assertEquals(nrResult.nrow, 9);
+  }
+
+}
