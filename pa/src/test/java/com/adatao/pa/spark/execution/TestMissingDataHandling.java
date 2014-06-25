5b060b6ddf95c0fb9f81de2e0907ffbf9ec04745
bhan <binhhan.gt@gmail.com>
2014-06-25 16:25:35 -0700

    test passed for fillNA executor

--- pa/src/test/java/com/adatao/pa/spark/execution/TestMissingDataHandling.java
+++ pa/src/test/java/com/adatao/pa/spark/execution/TestMissingDataHandling.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 import junit.framework.Assert;
 import org.apache.thrift.TException;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.adatao.pa.spark.DataManager.MetaInfo;
+import com.adatao.pa.spark.execution.Aggregate.AggregateResult;
 import com.adatao.pa.spark.execution.NRow.NRowResult;
 import com.adatao.pa.spark.types.ExecutionResult;
 import com.adatao.pa.thrift.generated.JsonCommand;
@@ -42,7 +44,7 @@ public class TestMissingDataHandling extends BaseTest {
   public void testDropNA() throws TException {
     JsonCommand cmd = new JsonCommand();
     JsonResult res;
-    com.adatao.pa.spark.Utils.DataFrameResult groupbyResult;
+    com.adatao.pa.spark.Utils.DataFrameResult dropNAResult;
     NRowResult nrResult;
     MetaInfo[] metaInfo;
 
@@ -50,19 +52,54 @@ public class TestMissingDataHandling extends BaseTest {
         .setSid(sid)
         .setCmdName("DropNA")
         .setParams(
-            String.format(
-                "{dataContainerID: %s," + "axis: row," + "how: any," + "thresh: 0," + "columns: null}",
+            String.format("{dataContainerID: %s," + "axis: row," + "how: any," + "thresh: 0," + "columns: null}",
                 airlineID)));
-    groupbyResult = ExecutionResult.fromJson(res.getResult(), com.adatao.pa.spark.Utils.DataFrameResult.class).result();
+    dropNAResult = ExecutionResult.fromJson(res.getResult(), com.adatao.pa.spark.Utils.DataFrameResult.class).result();
 
-    metaInfo = groupbyResult.getMetaInfo();
+    metaInfo = dropNAResult.getMetaInfo();
     LOG.info("DropNA result: " + Arrays.toString(metaInfo));
 
     Assert.assertTrue(metaInfo[0].getHeader().equals("year"));
     res = client.execJsonCommand(cmd.setSid(sid).setCmdName("NRow")
-        .setParams(String.format("{dataContainerID: %s}", groupbyResult.getDataContainerID())));
+        .setParams(String.format("{dataContainerID: %s}", dropNAResult.getDataContainerID())));
     nrResult = ExecutionResult.fromJson(res.getResult(), NRowResult.class).result();
     Assert.assertEquals(nrResult.nrow, 9);
+  }
+
+  @Test
+  public void testFillNA() throws TException {
+    JsonCommand cmd = new JsonCommand();
+    JsonResult res;
+    com.adatao.pa.spark.Utils.DataFrameResult fillNAResult;
+    NRowResult nrResult;
+    AggregateResult aggResult;
+    MetaInfo[] metaInfo;
+
+    res = client.execJsonCommand(cmd
+        .setSid(sid)
+        .setCmdName("FillNA")
+        .setParams(
+            String.format("{dataContainerID: %s," + "value: 1," + "method: null," + "limit: 0," + "function: null,"
+                + "columnsToValues: null," + "columns: null}", airlineID)));
+    fillNAResult = ExecutionResult.fromJson(res.getResult(), com.adatao.pa.spark.Utils.DataFrameResult.class).result();
+
+    metaInfo = fillNAResult.getMetaInfo();
+    LOG.info("FillNA result: " + Arrays.toString(metaInfo));
+
+    Assert.assertTrue(metaInfo[0].getHeader().equals("year"));
+    res = client.execJsonCommand(cmd.setSid(sid).setCmdName("NRow")
+        .setParams(String.format("{dataContainerID: %s}", fillNAResult.getDataContainerID())));
+    nrResult = ExecutionResult.fromJson(res.getResult(), NRowResult.class).result();
+    Assert.assertEquals(nrResult.nrow, 31);
+
+    res = client.execJsonCommand(cmd
+        .setSid(sid)
+        .setCmdName("Aggregate")
+        .setParams(
+            String.format("{dataContainerID: %s," + "colnames: [LateAircraftDelay]," + "groupBy: [year],"
+                + "func: sum}", fillNAResult.getDataContainerID())));
+    aggResult = ExecutionResult.fromJson(res.getResult(), AggregateResult.class).result();
+    Assert.assertEquals(aggResult.results.get("2008")[0], 301, 0.1);
   }
 
 }
