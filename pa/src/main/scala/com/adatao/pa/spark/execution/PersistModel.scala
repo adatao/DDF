8a457e9f282165cb407d86a43115e7a3249ff50a
huandao0812 <huan@adatau.com>
2014-06-29 16:43:29 +0700

    LoadModel and PersistModel to use uri

--- pa/src/main/scala/com/adatao/pa/spark/execution/PersistModel.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/PersistModel.scala
@@ -3,12 +3,11 @@ package com.adatao.pa.spark.execution
 /**
  * author: daoduchuan
  */
-class PersistModel(modelID: String, modelAlias: String) extends AExecutor[String] {
+class PersistModel(modelID: String) extends AExecutor[String] {
 
   override def runImpl(ctx: ExecutionContext): String = {
     val manager = ctx.sparkThread.getDDFManager;
     val model = manager.getModel(modelID)
-    model.setName(modelAlias)
 
     val ddf = model.serialize2DDF(manager)
     val uri = ddf.persist()
