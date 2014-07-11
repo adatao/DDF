c832b7002a9cded15b62caa5fde7ed1f4b3141f2
huandao0812 <huan@adatau.com>
2014-07-11 09:26:48 +0700

    add posfix -mesos

--- project/RootBuild.scala
+++ project/RootBuild.scala
@@ -38,7 +38,15 @@ object RootBuild extends Build {
   val rootOrganization = "com.adatao"
   val projectName = "ddf"
   val rootProjectName = projectName
-  val rootVersion = "0.9"
+
+  val YARN_ENABLED = env("SPARK_YARN").getOrElse("false").toBoolean
+
+  //val rootVersion = "0.9"
+  val rootVersion = if(YARN_ENABLED) {
+    "0.9"
+  } else {
+    "0.9-mesos"
+  }
 
   val projectOrganization = rootOrganization + "." + projectName
 
@@ -63,7 +71,7 @@ object RootBuild extends Build {
   val contribTestJarName = contribProjectName + "-" + contribVersion + "-tests.jar"
   
   lazy val root = Project("root", file("."), settings = rootSettings) aggregate(spark_adatao, examples, contrib,pa)
-  lazy val spark_adatao = Project("spark_adatao", file("spark_adatao"), settings = sparkSettings)
+  lazy val spark_adatao = Project("spark_adatao", file("spark_adatao"), settings = spark_adatao_Settings)
   lazy val pa = Project("pa", file("pa"), settings = paSettings)  dependsOn(spark_adatao)
   lazy val examples = Project("examples", file("examples"), settings = examplesSettings) dependsOn (spark_adatao)
   lazy val contrib = Project("contrib", file("contrib"), settings = contribSettings) dependsOn (spark_adatao)
@@ -107,36 +115,15 @@ object RootBuild extends Build {
   val scalaArtifacts = Seq("jline", "scala-compiler", "scala-library", "scala-reflect")
   val scalaDependencies = scalaArtifacts.map( artifactId => "org.scala-lang" % artifactId % theScalaVersion)
 
-  val spark_dependencies = Seq(
-    "commons-configuration" % "commons-configuration" % "1.6",
-    //"com.adatao.ddf" % "ddf_spark_2.10" % "0.9",
-    "com.google.code.gson"% "gson" % "2.2.2",
-    //"javax.jdo" % "jdo2-api" % "2.3-ec",
-//    "org.eclipse.jetty" % "jetty-server" % "8.1.14.v20131031",
-//    "org.eclipse.jetty" % "jetty-security" % "8.1.14.v20131031",
-//    "org.eclipse.jetty" % "jetty-util" % "8.1.14.v20131031",
-//    "org.eclipse.jetty" % "jetty-plus" % "8.1.14.v20131031",
-//    "org.eclipse.jetty" % "jetty-servlet" % "8.1.14.v20131031",
-//    "org.eclipse.jetty" % "jetty-webapp" % "8.1.14.v20131031",
-//    "org.eclipse.jetty" % "jetty-jsp" % "8.1.14.v20131031",
-    //"org.scalatest" %% "scalatest" % "1.9.1" % "test",
-    //"org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
-    "com.novocode" % "junit-interface" % "0.10" % "test",
-    "org.jblas" % "jblas" % "1.2.3", // for fast linear algebra
-    "org.apache.thrift" % "libthrift" % "0.9.0",
-    "org.apache.thrift" % "libfb303" % "0.9.0",
-    //"org.antlr" % "antlr" % "3.4", // needed by shark.SharkDriver.compile
-    // needed by Hive
-    //"commons-dbcp" % "commons-dbcp" % "1.4",
-    //"org.apache.derby" % "derby" % "10.4.2.0",
-   // "org.apache.spark" % "spark-streaming_2.10" % SPARK_VERSION excludeAll(excludeSpark),
-    "org.apache.spark" % "spark-core_2.10" % SPARK_VERSION excludeAll(excludeJets3t) exclude("com.google.protobuf", "protobuf-java") exclude("io.netty", "netty-all") exclude("org.jboss.netty", "netty"),
-    //"org.apache.spark" % "spark-repl_2.10" % SPARK_VERSION excludeAll(excludeSpark) exclude("com.google.protobuf", "protobuf-java") exclude("io.netty", "netty-all") exclude("org.jboss.netty", "netty"),
-    "org.apache.spark" % "spark-mllib_2.10" % SPARK_VERSION excludeAll(excludeSpark) exclude("io.netty", "netty-all") exclude("org.jboss.netty", "netty"),
-    "org.apache.spark" % "spark-yarn_2.10" % SPARK_VERSION,
-    //"edu.berkeley.cs.amplab" % "shark_2.9.3" % SHARK_VERSION excludeAll(excludeSpark)
-    "edu.berkeley.cs.shark" %% "shark" % SHARK_VERSION exclude("org.apache.avro", "avro-ipc") exclude("com.google.protobuf", "protobuf-java") exclude("io.netty", "netty-all"),
-    "com.google.protobuf" % "protobuf-java" % "2.5.0"
+//  val ddfSparkVersion = if(YARN_ENABLED) {
+//    rootVersion
+//  } else {
+//    rootVersion + "-mesos"
+//  }
+
+  val spark_adatao_dependencies = Seq(
+    "io.ddf" % "core_2.10" %  rootVersion,
+    "io.ddf" % "spark_2.10" % rootVersion
   )
 
   val pa_dependencies = Seq(
@@ -187,23 +174,6 @@ object RootBuild extends Build {
     publishMavenStyle := true, // generate pom.xml with "sbt make-pom"
 
     libraryDependencies ++= Seq(
-      "org.slf4j" % "slf4j-api" % slf4jVersion,
-      "org.slf4j" % "slf4j-log4j12" % slf4jVersion,
-      "com.adatao.ddf" % "ddf_core_2.10" %  rootVersion,
-      "com.adatao.ddf" % "ddf_spark_2.10" % rootVersion,
-      "commons-configuration" % "commons-configuration" % "1.6",
-      "com.google.guava" % "guava" % "14.0.1",
-      "com.google.code.gson"% "gson" % "2.2.2",
-      //"org.scalatest" % "scalatest_2.10" % "2.1.0" % "test",
-      "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
-      "org.scalacheck"   %% "scalacheck" % "1.10.0" % "test",
-      "com.novocode" % "junit-interface" % "0.10" % "test",
-      "org.jblas" % "jblas" % "1.2.3", // for fast linear algebra
-      "com.googlecode.matrix-toolkits-java" % "mtj" % "0.9.14",
-      "commons-io" % "commons-io" % "1.3.2",
-      "org.easymock" % "easymock" % "3.1" % "test",
-      //"edu.berkeley.cs.shark" % "hive-contrib" % "0.11.0-shark" exclude("com.google.protobuf", "protobuf-java") exclude("io.netty", "netty-all") exclude("org.jboss.netty", "netty"),
-      "mysql" % "mysql-connector-java" % "5.1.25"
     ),
 
 
@@ -474,7 +444,7 @@ object RootBuild extends Build {
 
 
 
-  def sparkSettings = commonSettings ++ Seq(
+  def spark_adatao_Settings = commonSettings ++ Seq(
     name := sparkProjectName,
     javaOptions in Test <+= baseDirectory map {dir => "-Dspark.classpath=" + dir + "/../lib_managed/jars/*"},
     // Add post-compile activities: touch the maven timestamp files so mvn doesn't have to compile again
@@ -486,7 +456,7 @@ object RootBuild extends Build {
       //"Cloudera Repository" at "https://repository.cloudera.com/artifactory/cloudera-repos/"
     ),
     libraryDependencies ++= com_adatao_unmanaged,
-    libraryDependencies ++= spark_dependencies
+    libraryDependencies ++= spark_adatao_dependencies
   ) ++ assemblySettings ++ extraAssemblySettings
 
 
