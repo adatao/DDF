bba017c73fcf59b76792731ca54f2b96a7d23181
root <root@ip-10-230-12-2.ec2.internal>
2014-08-25 04:41:08 +0000

    add bin/build-all.sh to build both opensource and enterprise

---
+++ bin/build_all.sh
@@ -0,0 +1,12 @@
+
+export DDF_ENTERPRISE_HOME="$(cd `dirname ${BASH_SOURCE[0]}`/../ >/dev/null 2>&1; echo $PWD)"
+echo $DDF_ENTERPRISE_HOME
+export DDF_OPENSOURCE_HOME=${DDF_ENTERPRISE_HOME}/../ddf-opensource
+echo $DDF_OPENSOURCE_HOME
+
+echo "building ddf-opensource"
+cd $DDF_OPENSOURCE_HOME
+bin/sbt clean compile package;bin/make-pom.sh; mvn install -DskipTests
+
+cd $DDF_ENTERPRISE_HOME
+pa/exe/build_and_deploy_jars.sh
