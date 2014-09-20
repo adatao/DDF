029838e68b6c08d4cd62c4b4b97f4a1c2d295893
huandao0812 <huan@adatau.com>
2014-09-20 17:34:45 +0700

    new BinaryConfusionMatrix design
    test passed

--- pa/src/test/scala/com/adatao/pa/spark/execution/MetricsSuite.scala
+++ pa/src/test/scala/com/adatao/pa/spark/execution/MetricsSuite.scala
@@ -39,34 +39,34 @@ class MetricsSuite extends ABigRClientTest {
     this.loadFile(List("resources/airline-transform.3.csv", "server/resources/airline-transform.3.csv"), false, ",")
   }
 
-//  test("Test YtrueYpredict function") {
-//
-//    createTableAdmission
-//    val df = this.runSQL2RDDCmd("select v3, v4, v1 from admission", true)
-//    val dataContainerId = df.dataContainerID
-//    val lambda = 0.0
-//
-//
-//    val trainer = new LogisticRegression(dataContainerId, Array(0, 1), 2, 1, 0.0, lambda, Array(37.285, -5.344, 1))
-//    val r = bigRClient.execute[IModel](trainer)
-//    assert(r.isSuccess)
-//
-//    val modelID = r.result.getName
-//
-//    //run prediction
-//    val predictor = new YtrueYpred(dataContainerId, modelID)
-//    val r2 = bigRClient.execute[DataFrameResult](predictor)
-//    val predictionResultId = r2.result.dataContainerID
-//    assert(r2.isSuccess)
-//
-//  }
-//  //
-//  //	/**
-//  //	 * this will test ROC execution
-//  //	 * success: if roc execution return success value
-//  //	 * fail: if not success
-//  //	 * for accuracy testing, please see MLMetricSuite
-//  //	 */
+  test("Test YtrueYpredict function") {
+
+    createTableAdmission
+    val df = this.runSQL2RDDCmd("select v3, v4, v1 from admission", true)
+    val dataContainerId = df.dataContainerID
+    val lambda = 0.0
+
+
+    val trainer = new LogisticRegression(dataContainerId, Array(0, 1), 2, 1, 0.0, lambda, Array(37.285, -5.344, 1))
+    val r = bigRClient.execute[IModel](trainer)
+    assert(r.isSuccess)
+
+    val modelID = r.result.getName
+
+    //run prediction
+    val predictor = new YtrueYpred(dataContainerId, modelID)
+    val r2 = bigRClient.execute[DataFrameResult](predictor)
+    val predictionResultId = r2.result.dataContainerID
+    assert(r2.isSuccess)
+
+  }
+  //
+  //	/**
+  //	 * this will test ROC execution
+  //	 * success: if roc execution return success value
+  //	 * fail: if not success
+  //	 * for accuracy testing, please see MLMetricSuite
+  //	 */
   test("Test ROC metric function") {
 
     createTableAdmission
@@ -106,195 +106,192 @@ class MetricsSuite extends ABigRClientTest {
 //    assert(truncate(metric.auc, 4) === 0.6743)
   }
 
-//  test("R2 metric is correct") {
-//    createTableMtcars
-//    val df = this.runSQL2RDDCmd("select wt, mpg from mtcars", true)
-//    val dataContainerId = df.dataContainerID
-//    val lambda = 0.0
-//
-//    // lm(mpg ~ wt, data=mtcars)
-//    // 37.285       -5.344
-//    // fake the training with learningRate = 0.0
-//    val trainer = new LinearRegression(dataContainerId, Array(0), 1, 1, 0.0, lambda, Array(37.285, -5.344))
-//    val r = bigRClient.execute[IModel](trainer)
-//    assert(r.isSuccess)
-//
-//    val modelID = r.result.getName
-//
-//
-//    val scorer = new R2Score(dataContainerId, modelID)
-//    val r2 = bigRClient.execute[Double](scorer)
-//    assert(r2.isSuccess)
-//
-//    // summary(lm(mpg ~ wt, data=mtcars))
-//    // Multiple R-squared:  0.7528
-//    assertEquals(0.7528, r2.result, 0.0001)
-//  }
-//  //
-//  test("Residuals metric is correct") {
-//    createTableMtcars
-//    val df = this.runSQL2RDDCmd("select wt, mpg from mtcars", true)
-//    val dataContainerId = df.dataContainerID
-//    val lambda = 0.0
-//
-//    val trainer = new LinearRegression(dataContainerId, Array(0), 1, 1, 0.0, lambda, Array(37.285, -5.344))
-//    val r = bigRClient.execute[IModel](trainer)
-//    assert(r.isSuccess)
-//
-//    val modelID = r.result.getName
-//
-//    val scorer = new Residuals(dataContainerId, modelID, Array(0), 1)
-//    val residuals = bigRClient.execute[DataFrameResult](scorer)
-//    assert(residuals.isSuccess)
-//
-//    val nrow = new NRow
-//    nrow.setDataContainerID(residuals.result.dataContainerID)
-//    val numRow = bigRClient.execute[NRowResult](nrow)
-//    assert(numRow.result.nrow === 32)
-//
-//  }
-//
-//  test("smoke test residuals metric") {
-//    createTableMtcars
-////    val df = this.runSQL2RDDCmd("select * from mtcars", true)
-//    val df = this.runSQL2RDDCmd("select v3, v4, v1 from admission", true)
-//    val dataContainerId = df.dataContainerID
-//    val lambda = 0.0
-//
-//    //minimum threshold range for sparse columns
-//    System.setProperty("sparse.max.range", "10000")
-//    var cmd2 = new FiveNumSummary(dataContainerId)
-//    val summary = bigRClient.execute[Array[ASummary]](cmd2).result
-//    assert(summary.size > 0)
-//
-//    //construct columnSummary parameter
-//    var columnsSummary = new HashMap[String, Array[Double]]
-//    var hmin = new Array[Double](summary.size)
-//    var hmax = new Array[Double](summary.size)
-//    //convert columnsSummary to HashMap
-//    var i = 0
-//    while (i < summary.size) {
-//      hmin(i) = summary(i).min
-//      hmax(i) = summary(i).max
-//      i += 1
-//    }
-//    columnsSummary.put("min", hmin)
-//    columnsSummary.put("max", hmax)
-//
-//    val trainer = new LogisticRegression(dataContainerId, Array(0, 1), 2, 1, 0.0, lambda, Array(37.285, -5.344, 1))
-//    val r = bigRClient.execute[IModel](trainer)
-//    assert(r.isSuccess)
-//
-//    val modelID = r.result.getName
-//
-//    val scorer = new Residuals(dataContainerId, modelID, Array(0), 1)
-//    val residuals = bigRClient.execute[DataFrameResult](scorer)
-//    assert(residuals.isSuccess)
-//
-//    println(">>>>>residuals =" + residuals.result)
-//
-//  }
+  test("R2 metric is correct") {
+    createTableMtcars
+    val df = this.runSQL2RDDCmd("select wt, mpg from mtcars", true)
+    val dataContainerId = df.dataContainerID
+    val lambda = 0.0
+
+    // lm(mpg ~ wt, data=mtcars)
+    // 37.285       -5.344
+    // fake the training with learningRate = 0.0
+    val trainer = new LinearRegression(dataContainerId, Array(0), 1, 1, 0.0, lambda, Array(37.285, -5.344))
+    val r = bigRClient.execute[IModel](trainer)
+    assert(r.isSuccess)
+
+    val modelID = r.result.getName
+
+
+    val scorer = new R2Score(dataContainerId, modelID)
+    val r2 = bigRClient.execute[Double](scorer)
+    assert(r2.isSuccess)
+
+    // summary(lm(mpg ~ wt, data=mtcars))
+    // Multiple R-squared:  0.7528
+    assertEquals(0.7528, r2.result, 0.0001)
+  }
   //
-//  test("can get linear predictions") {
-//    createTableMtcars
-//    val df = this.runSQL2RDDCmd("select wt, mpg from mtcars", true)
-//
-//    val dataContainerId = df.dataContainerID
-//    val lambda = 0.0
-//    // lm(mpg ~ wt, data=mtcars)
-//    // 37.285       -5.344
-//    // fake the training with learningRate = 0.0
-//    val trainer = new LinearRegression(dataContainerId, Array(0), 1, 1, 0.0, lambda, Array(37.285, -5.344))
-//    val r = bigRClient.execute[IModel](trainer)
-//    assert(r.isSuccess)
-//
-//    val persistenceID = r.result.getName
-//
-//    val predictor = new YtrueYpred(dataContainerId, persistenceID)
-//    val r2 = bigRClient.execute[DataFrameResult](predictor)
-//    assert(r2.isSuccess)
-//
-//  }
-//
-//  test("can get linear predictions categorical columns") {
-//    createTableAirline
-//
-//    val df = this.runSQL2RDDCmd("select v4, v17, v18, v3 from airline", true)
-//
-//    val dataContainerId = df.dataContainerID
-//
-//    val lambda = 0.0
-//    val trainer = new LinearRegression(dataContainerId, Array(0, 1, 2), 3, 50, 0.01, lambda, null)
-//    val r = bigRClient.execute[IModel](trainer)
-//
-//    assert(r.isSuccess)
-//
-//    val persistenceID = r.result.getName
-//
-//    val predictor = new YtrueYpred(dataContainerId, persistenceID)
-//    val r2 = bigRClient.execute[DataFrameResult](predictor)
-//    assert(r2.isSuccess)
-//
-//    //		val fetcher = new FetchRows().setDataContainerID(r2.result.dataContainerID).setLimit(32)
-//    //		val r3 = bigRClient.execute[FetchRowsResult](fetcher)
-//    //		assert(r3.isSuccess)
-//    //
-//    //		println(r3.result.data)
-//  }
-//  ////	//
-//  test("can get logistic predictions") {
-//
-//    createTableAdmission2
-//    val df = this.runSQL2RDDCmd("select * from admission", true)
-//    val dataContainerId = df.dataContainerID
-//
-//    val lambda = 0.0
-//
-//    // fake the training with learningRate = 0.0
-//    val trainer = new LogisticRegression(dataContainerId, Array(2, 3), 0, 1, 0.0, lambda, Array(-3.0, 1.5, -0.9))
-//    val r = bigRClient.execute[IModel](trainer)
-//    assert(r.isSuccess)
-//    val persistenceID = r.result.getName
-//
-//    val predictor = new YtrueYpred(dataContainerId, persistenceID)
-//    val r2 = bigRClient.execute[DataFrameResult](predictor)
-//    assert(r2.isSuccess)
-//
-//    //		val fetcher = new FetchRows().setDataContainerID(r2.result.dataContainerID).setLimit(32)
-//    //		val r3 = bigRClient.execute[FetchRowsResult](fetcher)
-//    //		assert(r3.isSuccess)
-//  }
-//
-//  test("test confusion matrix") {
-//
-//    createTableAdmission
-//    val df = this.runSQL2RDDCmd("select v3, v4, v1 from admission", true)
-//    val dataContainerId = df.dataContainerID
-//    val lambda = 0.0
-//
-//    // fake the training with learningRate = 0.0
-//    val trainer = new LogisticRegression(dataContainerId, Array(0, 1), 2, 1, 0.0, lambda, Array(-3.0, 1.5, -0.9))
-//    val r = bigRClient.execute[IModel](trainer)
-//    assert(r.isSuccess)
-//    val persistenceID = r.result.getName
-//
-//    println(">>>>model r.result = " + r.result)
-//
-//    val threshold = 0.5
-//    val executor = new BinaryConfusionMatrix(dataContainerId, persistenceID, threshold)
-//    val ret = bigRClient.execute[BinaryConfusionMatrixResult](executor)
-//    assert(ret.isSuccess)
-//
-//    val cm = ret.result
-//
-//    //		println(">>>>>>>>>>>>>>cm=" + cm.truePos + "\t" + cm.falsePos + "\t" + cm.trueNeg + "\t" + cm.falseNeg)
-//
-//    // TODO: need to double check these results by hand
-//    assert(cm.truePos === 79)
-//    assert(cm.falsePos === 102)
-//    assert(cm.falseNeg === 48)
-//    assert(cm.trueNeg === 171)
-//    assert(400 === cm.truePos + cm.falsePos + cm.falseNeg + cm.trueNeg) // total count
-//  }
-//
+  test("Residuals metric is correct") {
+    createTableMtcars
+    val df = this.runSQL2RDDCmd("select wt, mpg from mtcars", true)
+    val dataContainerId = df.dataContainerID
+    val lambda = 0.0
+
+    val trainer = new LinearRegression(dataContainerId, Array(0), 1, 1, 0.0, lambda, Array(37.285, -5.344))
+    val r = bigRClient.execute[IModel](trainer)
+    assert(r.isSuccess)
+
+    val modelID = r.result.getName
+
+    val scorer = new Residuals(dataContainerId, modelID, Array(0), 1)
+    val residuals = bigRClient.execute[DataFrameResult](scorer)
+    assert(residuals.isSuccess)
+
+    val nrow = new NRow
+    nrow.setDataContainerID(residuals.result.dataContainerID)
+    val numRow = bigRClient.execute[NRowResult](nrow)
+    assert(numRow.result.nrow === 32)
+
+  }
+
+  test("smoke test residuals metric") {
+    createTableMtcars
+//    val df = this.runSQL2RDDCmd("select * from mtcars", true)
+    val df = this.runSQL2RDDCmd("select v3, v4, v1 from admission", true)
+    val dataContainerId = df.dataContainerID
+    val lambda = 0.0
+
+    //minimum threshold range for sparse columns
+    System.setProperty("sparse.max.range", "10000")
+    var cmd2 = new FiveNumSummary(dataContainerId)
+    val summary = bigRClient.execute[Array[ASummary]](cmd2).result
+    assert(summary.size > 0)
+
+    //construct columnSummary parameter
+    var columnsSummary = new HashMap[String, Array[Double]]
+    var hmin = new Array[Double](summary.size)
+    var hmax = new Array[Double](summary.size)
+    //convert columnsSummary to HashMap
+    var i = 0
+    while (i < summary.size) {
+      hmin(i) = summary(i).min
+      hmax(i) = summary(i).max
+      i += 1
+    }
+    columnsSummary.put("min", hmin)
+    columnsSummary.put("max", hmax)
+
+    val trainer = new LogisticRegression(dataContainerId, Array(0, 1), 2, 1, 0.0, lambda, Array(37.285, -5.344, 1))
+    val r = bigRClient.execute[IModel](trainer)
+    assert(r.isSuccess)
+
+    val modelID = r.result.getName
+
+    val scorer = new Residuals(dataContainerId, modelID, Array(0), 1)
+    val residuals = bigRClient.execute[DataFrameResult](scorer)
+    assert(residuals.isSuccess)
+
+    println(">>>>>residuals =" + residuals.result)
+
+  }
+
+  test("can get linear predictions") {
+    createTableMtcars
+    val df = this.runSQL2RDDCmd("select wt, mpg from mtcars", true)
+
+    val dataContainerId = df.dataContainerID
+    val lambda = 0.0
+    // lm(mpg ~ wt, data=mtcars)
+    // 37.285       -5.344
+    // fake the training with learningRate = 0.0
+    val trainer = new LinearRegression(dataContainerId, Array(0), 1, 1, 0.0, lambda, Array(37.285, -5.344))
+    val r = bigRClient.execute[IModel](trainer)
+    assert(r.isSuccess)
+
+    val persistenceID = r.result.getName
+
+    val predictor = new YtrueYpred(dataContainerId, persistenceID)
+    val r2 = bigRClient.execute[DataFrameResult](predictor)
+    assert(r2.isSuccess)
+
+  }
+
+  test("can get linear predictions categorical columns") {
+    createTableAirline
+
+    val df = this.runSQL2RDDCmd("select v4, v17, v18, v3 from airline", true)
+
+    val dataContainerId = df.dataContainerID
+
+    val lambda = 0.0
+    val trainer = new LinearRegression(dataContainerId, Array(0, 1, 2), 3, 50, 0.01, lambda, null)
+    val r = bigRClient.execute[IModel](trainer)
+
+    assert(r.isSuccess)
+
+    val persistenceID = r.result.getName
+
+    val predictor = new YtrueYpred(dataContainerId, persistenceID)
+    val r2 = bigRClient.execute[DataFrameResult](predictor)
+    assert(r2.isSuccess)
+
+    //		val fetcher = new FetchRows().setDataContainerID(r2.result.dataContainerID).setLimit(32)
+    //		val r3 = bigRClient.execute[FetchRowsResult](fetcher)
+    //		assert(r3.isSuccess)
+    //
+    //		println(r3.result.data)
+  }
+  ////	//
+  test("can get logistic predictions") {
+
+    createTableAdmission2
+    val df = this.runSQL2RDDCmd("select * from admission", true)
+    val dataContainerId = df.dataContainerID
+
+    val lambda = 0.0
+
+    // fake the training with learningRate = 0.0
+    val trainer = new LogisticRegression(dataContainerId, Array(2, 3), 0, 1, 0.0, lambda, Array(-3.0, 1.5, -0.9))
+    val r = bigRClient.execute[IModel](trainer)
+    assert(r.isSuccess)
+    val persistenceID = r.result.getName
+
+    val predictor = new YtrueYpred(dataContainerId, persistenceID)
+    val r2 = bigRClient.execute[DataFrameResult](predictor)
+    assert(r2.isSuccess)
+
+    //		val fetcher = new FetchRows().setDataContainerID(r2.result.dataContainerID).setLimit(32)
+    //		val r3 = bigRClient.execute[FetchRowsResult](fetcher)
+    //		assert(r3.isSuccess)
+  }
+
+  test("test confusion matrix") {
+
+    createTableAdmission
+    val df = this.runSQL2RDDCmd("select v3, v4, v1 from admission", true)
+    val dataContainerId = df.dataContainerID
+    val lambda = 0.0
+
+    // fake the training with learningRate = 0.0
+    val trainer = new LogisticRegression(dataContainerId, Array(0, 1), 2, 1, 0.0, lambda, Array(-3.0, 1.5, -0.9))
+    val r = bigRClient.execute[IModel](trainer)
+    assert(r.isSuccess)
+    val persistenceID = r.result.getName
+
+    println(">>>>model r.result = " + r.result)
+
+    val threshold = 0.5
+    val executor = new BinaryConfusionMatrix(dataContainerId, persistenceID, threshold)
+    val ret = bigRClient.execute[BinaryConfusionMatrixResult](executor)
+    assert(ret.isSuccess)
+
+    val cm = ret.result
+
+    // TODO: need to double check these results by hand
+    assert(cm.truePos === 79)
+    assert(cm.falsePos === 102)
+    assert(cm.falseNeg === 48)
+    assert(cm.trueNeg === 171)
+    assert(400 === cm.truePos + cm.falsePos + cm.falseNeg + cm.trueNeg) // total count
+  }
 }
