552295b934f50a7d891952460c6b92a32b48c0a4
huandao0812 <huan@adatau.com>
2014-09-21 16:51:42 +0700

    fix-r2Score

--- pa/src/main/scala/com/adatao/pa/spark/execution/R2Score.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/R2Score.scala
@@ -34,29 +34,39 @@ import io.ddf.DDF
 class R2Score(var dataContainerID: String, var modelID: String) extends AExecutor[Double] {
 
   def runImpl(ctx: ExecutionContext): Double = {
-    val ddfManager = ctx.sparkThread.getDDFManager();
-    val ddfId = Utils.dcID2DDFID(dataContainerID)
-    val ddf: DDF = ddfManager.getDDF(ddfId);
+    val ddfManager = ctx.sparkThread.getDDFManager()
+    val model: IModel = ddfManager.getModel(modelID)
+
 
     // first, compute RDD[(ytrue, ypred)]
     //old API val predictions = getYtrueYpred(dataContainerID, modelID, xCols, yCol, ctx)
 
-    val model: IModel = ddfManager.getModel(modelID)
-    if (model == null) {
-      throw new AdataoException(AdataoExceptionCode.ERR_DATAFRAME_NONEXISTENT,
-        String.format("Not found dataframe with id %s", modelID), null);
-    }
-    val projectedDDF = ddf.VIEWS.project(model.getTrainedColumns: _*)
 
-    val predictionDDF = projectedDDF.getMLSupporter().applyModel(model, true, false)
-    if (predictionDDF == null) {
-      throw new AdataoException(AdataoExceptionCode.ERR_DATAFRAME_NONEXISTENT,
-        "Can not run prediction on dataContainerID: " + dataContainerID + "\t with modelID =" + modelID, null);
+//    if (model == null) {
+//      throw new AdataoException(AdataoExceptionCode.ERR_DATAFRAME_NONEXISTENT,
+//        String.format("Not found dataframe with id %s", modelID), null);
+//    }
+//    val projectedDDF = ddf.VIEWS.project(model.getTrainedColumns: _*)
+//
+//    val predictionDDF = projectedDDF.getMLSupporter().applyModel(model, true, false)
+//    if (predictionDDF == null) {
+//      throw new AdataoException(AdataoExceptionCode.ERR_DATAFRAME_NONEXISTENT,
+//        "Can not run prediction on dataContainerID: " + dataContainerID + "\t with modelID =" + modelID, null);
+//    }
+//    //get column mean
+//    val summary = projectedDDF.getStatisticsSupporter().getSummary()
+//    val yMean = summary(projectedDDF.getNumColumns - 1).mean()
+    model.getRawModel match {
+      case linearModel: ALinearModel[Double] => {
+        val ddf: DDF = ddfManager.getDDF(dataContainerID)
+        val projectedDDF = ddf.VIEWS.project(model.getTrainedColumns: _*)
+        val summary = projectedDDF.getStatisticsSupporter.getSummary
+        val yMean = summary(projectedDDF.getNumColumns - 1).mean()
+        val yTrueYPred = new YtrueYpred(dataContainerID, modelID).runImpl(ctx)
+        val predictionDDF = ddfManager.getDDF(yTrueYPred.getDataContainerID)
+        predictionDDF.getMLMetricsSupporter.r2score(yMean)
+      }
     }
-    //get column mean
-    val summary = projectedDDF.getStatisticsSupporter().getSummary()
-    val yMean = summary(projectedDDF.getNumColumns - 1).mean()
-
-    ddf.getMLMetricsSupporter().r2score(predictionDDF, yMean)
+    //ddf.getMLMetricsSupporter().r2score(predictionDDF, yMean)
   }
 }
