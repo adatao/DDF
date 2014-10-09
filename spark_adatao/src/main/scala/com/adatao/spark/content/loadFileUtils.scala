bad8e654aecc737fa1beffd9c8731d658418e6a9
huandao0812 <huan@adatau.com>
2014-10-09 12:19:19 +0700

    remove try catch block

--- spark_adatao/src/main/scala/com/adatao/spark/content/loadFileUtils.scala
+++ spark_adatao/src/main/scala/com/adatao/spark/content/loadFileUtils.scala
@@ -2,40 +2,43 @@ package com.adatao.spark.content
 
 import io.ddf.content.Schema
 import io.ddf.DDF
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.JavaConversions._
 
 import io.ddf.content.Schema.ColumnType
 import io.spark.ddf.{SparkDDF, SparkDDFManager}
 import org.apache.commons.lang.math.NumberUtils
+
 /**
   */
-  object LoadFileUtils {
+object LoadFileUtils {
 
   def loadFile(manager: SparkDDFManager, fileURL: String, schema: Schema, separator: String): DDF = {
     val rdd: RDD[String] = manager.getSparkContext.textFile(fileURL)
 
-    val rddArrObj: RDD[Array[Object]] = rdd.map{
+    val rddArrObj: RDD[Array[Object]] = rdd.map {
       row => {
         val arrStr = row.split(separator)
         val cols = schema.getColumns
         val arrObj = ArrayBuffer[Object]()
         var idx = 0
-        for(col <- cols) {
+        for (col <- cols) {
           arrObj += (col.getType match {
-            case ColumnType.INT => try {
-              (arrStr(idx).toInt).asInstanceOf[Object]
-            } catch {
-              case e: Throwable => null
+            case ColumnType.INT => {
+              if (NumberUtils.isNumber(arrStr(idx))) {
+                arrStr(idx).toInt.asInstanceOf[Object]
+              } else {
+                null
+              }
             }
-            case ColumnType.DOUBLE => try {
-              (arrStr(idx).toDouble).asInstanceOf[Object]
-            } catch {
-              case e: Throwable => null
+            case ColumnType.DOUBLE => {
+              if (NumberUtils.isNumber(arrStr(idx))) {
+                arrStr(idx).toDouble.asInstanceOf[Object]
+              } else {
+                null
+              }
             }
-            case ColumnType.STRING =>  try {
+            case ColumnType.STRING => try {
               arrStr(idx).asInstanceOf[Object]
             } catch {
               case e: Throwable => null
@@ -54,3 +57,13 @@ import org.apache.commons.lang.math.NumberUtils
     return ddf
   }
 }
+
+/*
+if (NumberUtils.isNumber (str) ) {
+number = Double.parseDouble (str)
+s.merge (number)
+}
+else {
+result (i) = null
+}
+*/
