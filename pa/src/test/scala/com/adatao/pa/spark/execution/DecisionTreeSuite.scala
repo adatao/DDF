03db737729ee1b70be152013c9e33e3e817252c6
huandao0812 <huan@adatau.com>
2014-11-11 02:01:41 -0800

    use mtcars for DecisionTreeSuite

--- pa/src/test/scala/com/adatao/pa/spark/execution/DecisionTreeSuite.scala
+++ pa/src/test/scala/com/adatao/pa/spark/execution/DecisionTreeSuite.scala
@@ -9,12 +9,13 @@ import io.ddf.ml.IModel
 class DecisionTreeSuite extends ABigRClientTest {
 
   test("test decision tree") {
-    createTableAirline
-    val cmd1 = new Sql2DataFrame("select * from airline", true)
+    createTableMtcars
+    val cmd1 = new Sql2DataFrame("select * from mtcars", true)
     val r1 = bigRClient.execute[Sql2DataFrame.Sql2DataFrameResult](cmd1).result
 
     val dataContainerId = r1.dataContainerID
-    val command = new DecisionTree(dataContainerId, Array(0,1,2,3,4,5), 21, "Classification")
+    val command = new DecisionTree(dataContainerId, Array(0,1,2,3,4,5), 7, "Classification",
+      maxDepth = 10)
     val model = bigRClient.execute[IModel](command).result
     assert(model != null)
   }
