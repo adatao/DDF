5437b5d5e2312faf1830cb2f6737bf2c12fc29f2
huandao0812 <huan@adatau.com>
2014-11-11 16:45:39 -0800

    add prediction for DecisionTree

--- pa/src/main/scala/com/adatao/pa/spark/execution/YtrueYpred.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/YtrueYpred.scala
@@ -16,6 +16,13 @@ import io.ddf.content.Schema
 import io.spark.ddf.SparkDDF
 import org.apache.spark.sql.SchemaRDD
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.tree.model.{DecisionTreeModel => DCModel}
+import io.spark.ddf.content.RepresentationHandler
+import org.apache.spark.mllib.linalg.Vectors
+import io.ddf.content.Schema
+import java.util
+import scala.util
+import io.ddf.content.Schema.Column
 
 /**
  * Return predictions pair (ytrue, ypred) RDD in a DataFrame,
@@ -45,6 +52,22 @@ class YtrueYpred(dataContainerID: String, val modelID: String) extends AExecutor
         val sparkDDF: DDF = ddfManager.newDDF(ddf.getManager, ytrueYpredRDD, Array[Class[_]](classOf[RDD[_]], classOf[Array[Double]]), ddf.getNamespace, null, schema)
         sparkDDF.getName()
         sparkDDF
+      }
+      case decisionTree: DCModel => {
+        val rddDouble = ddf.VIEWS.project(model.getTrainedColumns(): _*).getRepresentationHandler.
+          get(RepresentationHandler.RDD_ARR_DOUBLE.getTypeSpecsString).asInstanceOf[RDD[Array[Double]]]
+        val XsY = rddDouble.map{arr => (arr.take(arr.size - 1), arr(arr.size - 1))}
+        val vectorLabel = XsY.map{case (arr, y) => (Vectors.dense(arr), y)}
+        val rdd = vectorLabel.map{case (vec, y) => Array(y, decisionTree.predict(vec))}
+        val outputColumns = new java.util.ArrayList[Schema.Column]()
+        outputColumns.add(new Column("ytrue", "double"))
+        outputColumns.add(new Column("yPredict", "double"))
+        val schema: Schema = new Schema(null, outputColumns)
+        /**
+         * val resultDDF : DDF = this.getManager.newDDF(this.getManager, result.rdd, Array[Class[_]](classOf[Nothing],
+         * classOf[Array[Double]]), this.getManager.getNamespace, null, schema)
+         */
+        ddfManager.newDDF(ddfManager, rdd, Array(classOf[RDD[_]], classOf[Array[Double]]), ddfManager.getNamespace, null, schema)
       }
       case _ => {
         val projectedDDF = ddf.VIEWS.project(model.getTrainedColumns: _*)
