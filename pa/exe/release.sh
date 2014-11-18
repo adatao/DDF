15e875e960c038f683e74eee76327cb3078fb440
khangich <khangich@gmail.com>
2014-11-18 14:45:49 +0700

    update release script

--- pa/exe/release.sh
+++ pa/exe/release.sh
@@ -1,5 +1,5 @@
-#time_stamp=$(date +%Y-%m-%d-%T)
-#mkdir -p "/release/pa/${time_stamp}"
-#ln -s  "release/${time_stamp}" /release/pa/current
+time_stamp=$(date +%Y-%m-%d-%T)
+mkdir -p "/root/release/pa/${time_stamp}"
+ln -s  "/root/release/${time_stamp}" /release/pa/current
 #copy folder ddf-conf
 cp -r ../ddf-conf/* .
