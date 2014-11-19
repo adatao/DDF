f0ac2bd7f16d71dc304202bc48325aa492b16e37
nhan vu <nhanitvn@adatao.com>
2014-11-19 06:24:15 +0000

    comment out pa's CreateFolder executor due to its Java 1.6 incompatability

--- pa/src/main/scala/com/adatao/pa/spark/execution/CreateFolder.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/CreateFolder.scala
@@ -1,12 +1,12 @@
-package com.adatao.pa.spark.execution
+//package com.adatao.pa.spark.execution
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs._
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+//import org.apache.hadoop.conf.Configuration
+//import org.apache.hadoop.fs._
+//import java.io.IOException;
+//import java.nio.file.FileAlreadyExistsException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
 /**
  * Created with IntelliJ IDEA.
  * User: daoduchuan
@@ -14,32 +14,32 @@ import java.nio.file.Paths;
  * Time: 4:30 PM
  * To change this template use File | Settings | File Templates.
  */
-class CreateLocalFolder(tableName: String, dummy: String= null) extends AExecutor[String]{
+//class CreateLocalFolder(tableName: String, dummy: String= null) extends AExecutor[String]{
 
-	protected override def runImpl(context: ExecutionContext): String = {
-		val conf = new Configuration()
+//	protected override def runImpl(context: ExecutionContext): String = {
+//		val conf = new Configuration()
 
-		var storageFolder= System.getProperty("bigr.storage", CreateLocalFolder.DEFAUL_STORAGE)
-		if(storageFolder.endsWith("/"))
-			storageFolder=storageFolder.take(storageFolder.length - 1)
-		//have to create a dummy file inside folder
-		val outFolderPath= Paths.get(storageFolder +"/" + tableName +"/" + tableName)
-		Files.createDirectories(outFolderPath.getParent)
-		val path = storageFolder + "/" + tableName + "/"
+//		var storageFolder= System.getProperty("bigr.storage", CreateLocalFolder.DEFAUL_STORAGE)
+//		if(storageFolder.endsWith("/"))
+//			storageFolder=storageFolder.take(storageFolder.length - 1)
+//		//have to create a dummy file inside folder
+//		val outFolderPath= Paths.get(storageFolder +"/" + tableName +"/" + tableName)
+//		Files.createDirectories(outFolderPath.getParent)
+//		val path = storageFolder + "/" + tableName + "/"
 		/*
 		val out= fs.create(outFolder)
 		//delete dummy file
 		fs.delete(outFolder, false)
 		out.close() */
-		try{
-			Files.createFile(outFolderPath)
-		} catch {
-			case e: FileAlreadyExistsException => LOG.info("File already exists: " + e.getMessage)
-		}
-		path
-	}
-}
+//		try{
+//			Files.createFile(outFolderPath)
+//		} catch {
+//			case e: FileAlreadyExistsException => LOG.info("File already exists: " + e.getMessage)
+//		}
+//		path
+//	}
+//}
 
-object CreateLocalFolder{
-	val DEFAUL_STORAGE="/tmp/warehouse/"
-}
+//object CreateLocalFolder{
+//	val DEFAUL_STORAGE="/tmp/warehouse/"
+//}
