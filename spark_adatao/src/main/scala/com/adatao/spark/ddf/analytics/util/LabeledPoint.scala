17829188ad0dfcf4c6ae26f3a7f10c47683ae467
huandao0812 <huan@adatau.com>
2014-11-20 10:35:42 -0800

    remove TFIDF & NaiveBayes

--- spark_adatao/src/main/scala/com/adatao/spark/ddf/analytics/util/LabeledPoint.scala
+++
@@ -1,9 +0,0 @@
-package com.adatao.spark.ddf.analytics.util
-
-import com.adatao.spark.ddf.analytics.util.Vector
-
-case class LabeledPoint(label: Double, features: Vector) {
-  override def toString: String = {
-    "(%s,%s)".format(label, features)
-  }
-}
