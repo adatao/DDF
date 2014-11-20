9c0b973b467943935d6337d6610440e716dcfa6b
huandao0812 <huan@adatau.com>
2014-11-20 10:16:20 -0800

    fix assembly.xml, conf/distribution/linux/pa-env.sh

--- pa/conf/distribution/linux/pa-env.sh
+++ pa/conf/distribution/linux/pa-env.sh
@@ -50,24 +50,21 @@ export TMP_DIR=/tmp # this where pAnalytics server stores temporarily files
 export LOG_DIR=/tmp # this where pAnalytics server stores log files
 export SPARK_HOME=${PA_HOME}/exe/
 export PA_PORT=7911
-export HADOOP_CONF_DIR=/root/hadoop-2.2.0.2.0.6.0-101/conf
-export HIVE_CONF_DIR=/root/hive-0.9.0-bin/conf #${PA_HOME}/conf/hive-conf
+export HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-/root/hadoop-2.2.0.2.0.6.0-101/conf}
+
 export RLIBS="${PA_HOME}/rlibs"
 export RSERVE_LIB_DIR="${RLIBS}/Rserve/libs/"
-export RSERVER_JAR=`find ${PA_HOME}/ -name ddf_pa_ass*.jar | grep -v '\-tests.jar'`
-export DDF_SPARK_JAR=`find ${PA_HOME}/../spark_adatao/ -name ddf_spark_adatao*.jar | grep -v '\-tests.jar'`
+export RSERVER_JAR=`find ${PA_HOME}/ -name ddf_pa_*.jar | grep -v '\-tests.jar'`
+export DDFSPARK_JAR=`find ${PA_HOME}/../spark_adatao/ -name ddf_spark_adatao-assembly*.jar | grep -v '\-tests.jar'`
 echo RSERVER_JAR=$RSERVER_JAR
-echo DDF_SPARK_JAR=$DDF_SPARK_JAR
+echo DDFSPARK_JAR=$DDFSPARK_JAR
 SPARK_CLASSPATH=$RSERVER_JAR
-#SPARK_CLASSPATH+=:"$DDF_CORE_JAR"
-#SPARK_CLASSPATH+=:"$DDF_SPARK_JAR"
-#SPARK_CLASSPATH+=:"${PA_HOME}/../lib_managed/jars/*"
-#SPARK_CLASSPATH+=:"${PA_HOME}/../lib_managed/bundles/*"
-#SPARK_CLASSPATH+=:"${PA_HOME}/../lib_managed/orbits/*"
-SPARK_CLASSPATH+=:"${PA_HOME}/libs/jars/*"
-SPARK_CLASSPATH+=:"${PA_HOME}/libs/bundles/*"
-SPARK_CLASSPATH+=:"${PA_HOME}/libs/orbits/*"
-SPARK_CLASSPATH+=:"${PA_HOME}/conf/"
+
+SPARK_CLASSPATH+=:"$DDFSPARK_JAR"
+SPARK_CLASSPATH+=:"${PA_HOME}/../lib_managed/jars/*"
+SPARK_CLASSPATH+=:"${PA_HOME}/../lib_managed/bundles/*"
+SPARK_CLASSPATH+=:"${PA_HOME}/../lib_managed/orbits/*"
+SPARK_CLASSPATH+=:"${PA_HOME}/conf/distributed/"
 
 #The order of the following two lines is important please dont change
 SPARK_CLASSPATH+=":${HIVE_CONF_DIR}"
@@ -77,28 +74,41 @@ export SPARK_CLASSPATH
 SPARK_JAVA_OPTS="-Dspark.storage.memoryFraction=0.6"
 SPARK_JAVA_OPTS+=" -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
 SPARK_JAVA_OPTS+=" -Dspark.serializer=org.apache.spark.serializer.KryoSerializer -Dspark.kryo.registrator=io.spark.content.KryoRegistrator"
-SPARK_JAVA_OPTS+=" -Dlog4j.configuration=pa-log4j.properties "
+SPARK_JAVA_OPTS+=" -Dlog4j.configuration=pa-log4j.properties"
 SPARK_JAVA_OPTS+=" -Dspark.local.dir=${TMP_DIR}"
 SPARK_JAVA_OPTS+=" -Dspark.ui.port=30001"
 SPARK_JAVA_OPTS+=" -Djava.io.tmpdir=${TMP_DIR}"
 SPARK_JAVA_OPTS+=" -Dspark.kryoserializer.buffer.mb=125"
-SPARK_JAVA_OPTS+=" -Dspark.executor.memory=${SPARK_MEM}"
+SPARK_JAVA_OPTS+=" -Dspark.executor.memory=${SPARK_MEMORY}"
+SPARK_JAVA_OPTS+=" -Dspark.driver.memory=${SPARK_MEMORY}"
+SPARK_JAVA_OPTS+=" -Dspark.sql.inMemoryColumnarStorage.compressed=true"
+SPARK_JAVA_OPTS+=" -Dspark.sql.inMemoryColumnarStorage.batchSize=1000000"
+SPARK_JAVA_OPTS+=" -Dspark.akka.heartbeat.interval=3"
 SPARK_JAVA_OPTS+=" -Dbigr.Rserve.split=1"
 SPARK_JAVA_OPTS+=" -Dbigr.multiuser=false"
-#SPARK_JAVA_OPTS+=" -Dpa.keytab.file=${PA_HOME}/conf/pa.keytab"
+SPARK_JAVA_OPTS+=" -Dspark.shuffle.manager=sort"
+SPARK_JAVA_OPTS+=" -Dspark.worker.reconnect.interval=10"
+#SPARK_JAVA_OPTS+=" -Dpa.keytab.file=${PA_HOME}/conf/pa.keytabs"
 #SPARK_JAVA_OPTS+=" -Dpa.authentication=true"
-#SPARK_JAVA_OPTS+=" -Dpa.user=pa"
+#SPARK_JAVA_OPTS+=" -Dpa.admin.user=pa"
+#SPARK_JAVA_OPTS+=" -Drun.as.admin=true"
 #SPARK_JAVA_OPTS+=" -Dsun.security.krb5.debug=true"
 export SPARK_JAVA_OPTS
 if [ "X$cluster" == "Xyarn" ]; then
         echo "Running pAnalytics with Yarn"
         export SPARK_MASTER="yarn-client"
-        export SPARK_WORKER_INSTANCES=20
+        export SPARK_WORKER_INSTANCES=12
         export SPARK_WORKER_CORES=8
-        export SPARK_WORKER_MEMORY=$SPARK_MEM
+        export SPARK_WORKER_MEMORY=$SPARK_MEMORY
+        export SPARK_DRIVER_MEMORY=$SPARK_MEMORY
         export SPARK_JAR=`find ${PA_HOME}/ -name ddf_pa-assembly-*.jar`
-        export HADOOP_NAMENODE=`cat ${HADOOP_CONF_DIR}/masters`
-        export SPARK_YARN_APP_JAR=hdfs:///user/root/ddf_pa-assembly-0.9.jar
+        echo SPARK_JAR=$SPARK_JAR
+        export SPARK_YARN_APP_JAR=hdfs:///user/root/ddf_pa_2.10-1.2.0.jar
+        
+        SPARK_CLASSPATH+=:"${PA_HOME}/conf/distributed/"
+         
+        #export SPARK_JAR=`find ${PA_HOME}/ -name ddf_pa-assembly-*.jar`
+        #export SPARK_YARN_APP_JAR=hdfs:///user/root/ddf_pa-assembly-0.9.jar
         [ "X$SPARK_YARN_APP_JAR" == "X" ] && echo "Please define SPARK_YARN_APP_JAR" && exit 1
         [ "X$HADOOP_CONF_DIR" == "X" ] && echo "Please define HADOOP_CONF_DIR" && exit 1
         [ "X$SPARK_WORKER_INSTANCES" == "X" ] && echo "Notice! SPARK_WORKER_INSTANCES is not defined, the default value will be used instead"
@@ -111,7 +121,11 @@ elif [ "X$cluster" == "Xspark" ]; then
         #export SPARK_MASTER= #spark://<host>:<port>
 elif [ "X$cluster" == "Xlocalspark" ]; then
         echo "Running pAnalytics with Spark in local node"
-        export SPARK_MEM=512m
-        export SPARK_WORKER_MEMORY=$SPARK_MEM
-        export SPARK_MASTER=spark://localhost:7070
+        export SPARK_MEM=$SPARK_MEMORY
+       # export SPARK_WORKER_MEMORY=$SPARK_MEMORY
+        export SPARK_MASTER=local
+        SPARK_JAVA_OPTS+=" -Dlog4j.configuration=pa-local-log4j.properties" 
+        #SPARK_CLASSPATH+=:"pa-local-log4j.properties"
+        SPARK_CLASSPATH+=:"${PA_HOME}/conf/local/"
 fi
+export SPARK_CLASSPATH
