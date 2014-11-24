50bfd2c2cc10185f0f8fe77a23adf507d3af571a
huandao0812 <huan@adatau.com>
2014-11-24 15:45:47 -0800

    use insertInto to persist DDF

--- pa/src/main/scala/com/adatao/pa/spark/execution/PersistDDF.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/PersistDDF.scala
@@ -3,7 +3,8 @@ package com.adatao.pa.spark.execution
 import io.spark.ddf.SparkDDFManager
 import io.spark.ddf.content.RepresentationHandler
 import org.apache.spark.sql.SchemaRDD
-
+import io.ddf.DDF
+import scala.collection.JavaConversions._
 /**
  */
 
@@ -12,8 +13,17 @@ class PersistDDF(dataContainerID: String, tableName: String) extends AExecutor[U
   override def runImpl(ctx: ExecutionContext): Unit = {
     val manager = ctx.sparkThread.getDDFManager.asInstanceOf[SparkDDFManager]
     val ddf = manager.getDDF(dataContainerID)
-    val ddfName = ddf.getTableName
-    val schemardd = manager.getHiveContext.sql(s"select * from $ddfName")
-    schemardd.saveAsTable(tableName)
+    val hiveSchema = PersistDDF.createHiveSchema(ddf)
+    manager.getHiveContext.sql(s"create table ${tableName} ($hiveSchema})")
+    val schemaRDD = ddf.getRepresentationHandler.get(RepresentationHandler.SCHEMARDD.getTypeSpecsString).asInstanceOf[SchemaRDD]
+    schemaRDD.insertInto(tableName)
+  }
+}
+
+object PersistDDF {
+  def createHiveSchema(ddf: DDF): String = {
+    ddf.getSchema.getColumns.map{
+      column => s"${column.getName} ${column.getType.toString}"
+    }.mkString(", ")
   }
 }
