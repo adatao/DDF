db8a10303148f4bbdf2de41cc692ca443794f94f
huandao0812 <huan@adatau.com>
2014-11-28 16:51:28 -0800

    fix ListPersistedDDF uri

--- pa/src/main/scala/com/adatao/pa/spark/execution/ListPersistedDDF.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/ListPersistedDDF.scala
@@ -3,12 +3,12 @@ import io.spark.ddf.content.PersistenceHandler
 /**
  * author: daoduchuan
  */
-class ListPersistedDDF extends AExecutor[java.util.List[String]] {
+class ListPersistedDDF extends AExecutor[List[String]] {
 
-  override def runImpl(ctx: ExecutionContext): java.util.List[String] = {
+  override def runImpl(ctx: ExecutionContext): List[String] = {
 
     val manager = ctx.sparkThread.getDDFManager
     val dummyDDF = manager.newDDF()
-    dummyDDF.getPersistenceHandler.asInstanceOf[PersistenceHandler].listPersistedDDF
+    dummyDDF.getPersistenceHandler.asInstanceOf[PersistenceHandler].listPersistedDDFUris
   }
 }
