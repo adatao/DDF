d55fcf0531f8bc0e0219359a8b7d693dc9763356
huandao0812 <huan@adatau.com>
2014-12-05 16:59:17 -0800

    add test for TransformDummy

--- spark_adatao/src/test/scala/ATestSuite.scala
+++ spark_adatao/src/test/scala/ATestSuite.scala
@@ -105,11 +105,11 @@ abstract class ATestSuite extends FunSuite with BeforeAndAfterEach with BeforeAn
     manager.sql2txt("LOAD DATA LOCAL INPATH '${hiveconf:shark.test.data.path}/TransformTest.csv' INTO TABLE kmeans")
   }
 
-  def createTableTransform() = {
+  def createTableTransformTest() = {
     manager.sql2txt("set shark.test.data.path=../pa/resources")
-    manager.sql2txt("drop table if exists kmeans")
-    manager.sql2txt("CREATE TABLE kmeans (v1 double, v2 double) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
-    manager.sql2txt("LOAD DATA LOCAL INPATH '${hiveconf:shark.test.data.path}/TransformTest2.csv' INTO TABLE kmeans")
+    manager.sql2txt("drop table if exists transformTest")
+    manager.sql2txt("CREATE TABLE transformTest (v1 double, v2 double) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
+    manager.sql2txt("LOAD DATA LOCAL INPATH '${hiveconf:shark.test.data.path}/TransformTest2.csv' INTO TABLE transformTest")
   }
 
   def createTableAirlineBig() {
