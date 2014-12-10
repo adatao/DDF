fe7738e6daeeab1d33f7467a02db1cf0f75ca26e
huandao0812 <huan@adatau.com>
2014-12-09 19:15:54 -0800

    debugging

--- pa/src/main/scala/com/adatao/pa/spark/execution/CosineSimilarity.scala
+++ pa/src/main/scala/com/adatao/pa/spark/execution/CosineSimilarity.scala
@@ -38,6 +38,8 @@ class CosineSimilarity(dataContainerID1: String, dataContainerID2: String, val t
     val vertice2BF = CosineSimilarity.createBloomFilter(vertices2)
     LOG.info("vertice1BF.size = " + vertice1BF.size.estimate)
     LOG.info("vertice2BF.size = " + vertice2BF.size.estimate)
+    LOG.info("vertices1.size = " + vertices1.count())
+    LOG.info("vertices2.size = " + vertices2.count())
 
     val broadcastVBF1 = sparkCtx.broadcast(vertice1BF)
     val broadcastVBF2 = sparkCtx.broadcast(vertice2BF)
