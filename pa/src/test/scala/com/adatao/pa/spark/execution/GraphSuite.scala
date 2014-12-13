234cd83aaa7b684ba54f8a2452c7a82122ab64fa
huandao0812 <huan@adatau.com>
2014-12-12 23:50:51 -0800

    add assert for new graphtfidf

--- pa/src/test/scala/com/adatao/pa/spark/execution/GraphSuite.scala
+++ pa/src/test/scala/com/adatao/pa/spark/execution/GraphSuite.scala
@@ -29,25 +29,25 @@ class GraphSuite extends ABigRClientTest {
     result.map{
       row => println(row.mkString(", "))
     }
-//    assert(result(0)(0) == "SFO")
-//    assert(result(0)(1) == "SAN")
-//    assert(result(0)(2) == 1.2148111519023101)
-//
-//    assert(result(1)(0) == "SFO")
-//    assert(result(1)(1) == "SJC")
-//    assert(result(1)(2) == 0.20827853703164503)
-//
-//    assert(result(2)(0) == "SMF")
-//    assert(result(2)(1) == "LAX")
-//    assert(result(2)(2) == 1.0123426265852582)
-//
-//    assert(result(3)(0) == "SMF")
-//    assert(result(3)(1) == "SJC")
-//    assert(result(3)(2) == 0.3471308950527417)
-//
-//    assert(result(5)(0) == "SNA")
-//    assert(result(5)(1) == "OAK")
-//    assert(result(5)(2) == 0.8282803308424841)
+    assert(result(0)(0) == "-3351804022671213993")
+    assert(result(0)(1) == "-3351804022671214149")
+    assert(result(0)(2) == 1.2148111519023101)
+
+    assert(result(1)(0) == "-3351804022671213993")
+    assert(result(1)(1) == "-3351804022671213881")
+    assert(result(1)(2) == 0.20827853703164503)
+
+    assert(result(2)(0) == "-3351804022671213785")
+    assert(result(2)(1) == "-3351804022671220866")
+    assert(result(2)(2) == 1.0123426265852582)
+
+    assert(result(3)(0) == "-3351804022671213785")
+    assert(result(3)(1) == "-3351804022671213881")
+    assert(result(3)(2) == 0.3471308950527417)
+
+    assert(result(5)(0) == "-3351804022671213759")
+    assert(result(5)(1) == "-3351804022671217996")
+    assert(result(5)(2) == 0.8282803308424841)
   }
 
   ignore("test Cosine Similarity") {
